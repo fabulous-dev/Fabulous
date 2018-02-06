6ca231b760bb160c54075cfe3ad00eac2dfc4185
Don Syme <dsyme@microsoft.com>
2018-02-06 12:36:05 +0000

    MasterDetail sample first pass

---
+++ Samples/MasterDetailApp/MasterDetailApp/MasterDetailApp.fs
@@ -0,0 +1,45 @@
+﻿namespace MasterDetailApp
+
+open Elmish
+open Elmish.XamarinForms
+open Xamarin.Forms
+
+type Model = 
+  { Count : int
+    Step : int }
+
+type Msg = 
+    | Increment 
+    | Decrement 
+    | Reset
+    | SetStep of int
+
+type MasterDetailApp () = 
+    inherit Application ()
+
+    let init () = { Count = 0; Step = 3 }
+
+    let update msg model =
+        match msg with
+        | Increment -> { model with Count = model.Count + model.Step }
+        | Decrement -> { model with Count = model.Count - model.Step }
+        | Reset -> init ()
+        | SetStep n -> { model with Step = n }
+
+    let view _ _ =
+        [ "CounterValue" |> Binding.oneWay (fun m -> m.Count)
+          "CounterValue2" |> Binding.oneWay (fun m -> m.Count + 1)
+          "IncrementCommand" |> Binding.cmd (fun _ _ -> Increment)
+          "DecrementCommand" |> Binding.cmd (fun _ _ -> Decrement)
+          "ResetCommand" |> Binding.cmdIf (fun _ _ -> Reset) (fun _ m -> m <> init ())
+          "ResetVisible" |> Binding.oneWay (fun m ->  m <> init ())
+          "StepValue" |> Binding.twoWay (fun m -> double m.Step) (fun v m -> v |> ((+)0.5) |> int |> SetStep) ]
+
+    let page = MasterDetailApp.CounterPage ()
+
+    do
+        Program.mkSimple init update view
+        |> Program.withConsoleTrace
+        |> Program.runPage page
+
+        base.MainPage <- page
