7b5313002b296b1729a19cf576ae99bcfd5b5b14
Don Syme <dsyme@microsoft.com>
2018-04-30 16:23:59 +0100

    add timer to CounterApp sample

--- Samples/CounterApp/CounterApp/CounterApp.fs
+++ Samples/CounterApp/CounterApp/CounterApp.fs
@@ -8,25 +8,36 @@ open Xamarin.Forms
 
 type Model = 
   { Count : int
-    Step : int }
+    Step : int
+    TimerOn: bool }
 
 type Msg = 
     | Increment 
     | Decrement 
     | Reset
     | SetStep of int
+    | TimerToggled of bool
+    | TimedTick
 
 type CounterApp () = 
     inherit Application ()
 
-    let init () = { Count = 0; Step = 3 }
+    let initModel = { Count = 0; Step = 1; TimerOn=false }
+    let init () = initModel, Cmd.none
+
+    let timerCmd = 
+       async { do! Async.Sleep 200
+               return TimedTick }
+       |> Cmd.ofAsyncMsg
 
     let update msg model =
         match msg with
-        | Increment -> { model with Count = model.Count + model.Step }
-        | Decrement -> { model with Count = model.Count - model.Step }
+        | Increment -> { model with Count = model.Count + model.Step }, Cmd.none
+        | Decrement -> { model with Count = model.Count - model.Step }, Cmd.none
         | Reset -> init ()
-        | SetStep n -> { model with Step = n }
+        | SetStep n -> { model with Step = n }, Cmd.none
+        | TimerToggled on -> { model with TimerOn = on }, (if on then timerCmd else Cmd.none)
+        | TimedTick -> if model.TimerOn then { model with Count = model.Count + model.Step }, timerCmd else model, Cmd.none
 
     let view (model: Model) dispatch =
       Xaml.ContentPage(
@@ -34,20 +45,22 @@ type CounterApp () =
           children=[ 
             yield Xaml.StackLayout(padding=20.0, verticalOptions=LayoutOptions.Center,
               children=[
-                 Xaml.Label(text= sprintf "%d" model.Count, horizontalOptions=LayoutOptions.Center, textColor=Color.Blue)
-                 Xaml.Label(text= sprintf "%d" (model.Count + 100), horizontalOptions=LayoutOptions.Center, textColor=Color.Green)
+                 Xaml.Label(text= sprintf "%d" model.Count, horizontalOptions=LayoutOptions.Center, fontSize = "Large")
                  Xaml.Button(text="Increment", command= (fun () -> dispatch Increment))
                  Xaml.Button(text="Decrement", command= (fun () -> dispatch Decrement))
+                 Xaml.StackLayout(padding=20.0, orientation=StackOrientation.Horizontal, horizontalOptions=LayoutOptions.Center,
+                                  children = [ Xaml.Label(text="Timer"); 
+                                               Xaml.Switch(isToggled=model.TimerOn, toggled = (fun on -> dispatch (TimerToggled on.Value))) ])
                  Xaml.Slider(minimum=0.0, maximum=10.0, value= double model.Step, valueChanged=(fun args -> dispatch (SetStep (int (args.NewValue + 0.5)))))
                  Xaml.Label(text=sprintf "Step size: %d" model.Step, horizontalOptions=LayoutOptions.Center) 
               ])
-            if model <> init() then 
+            if model <> initModel then 
                 yield Xaml.Button(text="Reset", horizontalOptions=LayoutOptions.Center, command= (fun () -> dispatch Reset))
           ]))
 
     do
         let page = 
-            Program.mkSimple init update view
+            Program.mkProgram init update view
             |> Program.withConsoleTrace
             |> Program.withDynamicView
             |> Program.run
