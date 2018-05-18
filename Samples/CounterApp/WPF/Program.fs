d197e096487a0150c71f5de4f3bb8ce095431f2d
Gibran Rosa <gibran.santos@gmail.com>
2018-05-18 16:59:15 -0300

    Added WPF target project to Counter Sample

---
+++ Samples/CounterApp/WPF/Program.fs
@@ -0,0 +1,17 @@
+﻿open Xamarin.Forms.Platform.WPF
+open Xamarin.Forms
+open System
+
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+type MainWindow() as this =
+    inherit FormsApplicationPage()
+
+    do Forms.Init()
+    do this.LoadApplication(new CounterApp.CounterApp())
+
+[<STAThread>] 
+[<EntryPoint>]
+let main argv = 
+    (new System.Windows.Application()).Run(MainWindow()) // return an integer exit code
