ded7386177721b7901d3d696b59e0a966ec9c84a
Timothé Larivière <timothe.lariviere@outlook.com>
2018-08-06 10:06:36 +0200

    AssemblyInfo

--- Elmish.XamarinForms/AssemblyInfo.fs
+++ Elmish.XamarinForms/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Elmish.XamarinForms")>]
 [<assembly: AssemblyProductAttribute("Elmish.XamarinForms")>]
 [<assembly: AssemblyDescriptionAttribute("F# Functional App Dev Framework")>]
-[<assembly: AssemblyVersionAttribute("0.14.8")>]
-[<assembly: AssemblyFileVersionAttribute("0.14.8")>]
+[<assembly: AssemblyVersionAttribute("0.16.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.16.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Elmish.XamarinForms"
     let [<Literal>] AssemblyProduct = "Elmish.XamarinForms"
     let [<Literal>] AssemblyDescription = "F# Functional App Dev Framework"
-    let [<Literal>] AssemblyVersion = "0.14.8"
-    let [<Literal>] AssemblyFileVersion = "0.14.8"
+    let [<Literal>] AssemblyVersion = "0.16.0"
+    let [<Literal>] AssemblyFileVersion = "0.16.0"
--- extensions/SkiaSharp/AssemblyInfo.fs
+++ extensions/SkiaSharp/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Elmish.XamarinForms.SkiaSharp")>]
 [<assembly: AssemblyProductAttribute("Elmish.XamarinForms.SkiaSharp")>]
 [<assembly: AssemblyDescriptionAttribute("Elmish.XamarinForms extension for SkiaSharp")>]
-[<assembly: AssemblyVersionAttribute("0.14.8")>]
-[<assembly: AssemblyFileVersionAttribute("0.14.8")>]
+[<assembly: AssemblyVersionAttribute("0.16.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.16.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Elmish.XamarinForms.SkiaSharp"
     let [<Literal>] AssemblyProduct = "Elmish.XamarinForms.SkiaSharp"
     let [<Literal>] AssemblyDescription = "Elmish.XamarinForms extension for SkiaSharp"
-    let [<Literal>] AssemblyVersion = "0.14.8"
-    let [<Literal>] AssemblyFileVersion = "0.14.8"
+    let [<Literal>] AssemblyVersion = "0.16.0"
+    let [<Literal>] AssemblyFileVersion = "0.16.0"
