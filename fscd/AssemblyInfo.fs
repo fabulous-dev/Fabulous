c7e975dd13a0e605613bfdfb5824565dc5a450c1
Timothé LARIVIERE <timothe.lariviere@outlook.com>
2018-08-30 19:42:39 +0200

    AssemblyInfo

--- fscd/AssemblyInfo.fs
+++ fscd/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("fscd")>]
 [<assembly: AssemblyProductAttribute("fscd")>]
 [<assembly: AssemblyDescriptionAttribute("F# Compiler Daemon")>]
-[<assembly: AssemblyVersionAttribute("0.16.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.16.0")>]
+[<assembly: AssemblyVersionAttribute("0.17.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.17.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "fscd"
     let [<Literal>] AssemblyProduct = "fscd"
     let [<Literal>] AssemblyDescription = "F# Compiler Daemon"
-    let [<Literal>] AssemblyVersion = "0.16.0"
-    let [<Literal>] AssemblyFileVersion = "0.16.0"
+    let [<Literal>] AssemblyVersion = "0.17.0"
+    let [<Literal>] AssemblyFileVersion = "0.17.0"
