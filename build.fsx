53a0fa38c1f521e25132ee5e09de57dd3bf9caea
Timothé LARIVIERE <timothe.lariviere@outlook.com>
2018-10-09 19:11:25 +0200

    Added RunGenerator as part of the build process

--- build.fsx
+++ build.fsx
@@ -50,12 +50,12 @@ let removeIncompatiblePlatformProjects pattern =
         pattern
 
 let projects = [
-    { Name = "Tools";       Path = !! "tools/**/*.fsproj";      Action = MSBuild Release;    OutputPath = buildDir + "/tools" }
     { Name = "Src";         Path = !! "src/**/*.fsproj";        Action = DotNetPack;         OutputPath = buildDir }
     { Name = "Extensions";  Path = !! "extensions/**/*.fsproj"; Action = DotNetPack;         OutputPath = buildDir }
     { Name = "Tests";       Path = !! "tests/**/*.fsproj";      Action = MSBuild Release;    OutputPath = buildDir + "/tests" }
     { Name = "Templates";   Path = !! "templates/**/*.nuspec";  Action = NuGetPack;          OutputPath = buildDir }
 ]
+let tools = { Name = "Tools"; Path = !! "tools/**/*.fsproj"; Action = MSBuild Release; OutputPath = buildDir + "/tools" }
 let samples = { Name = "Samples"; Path = (!! "samples/**/*.fsproj" |> removeIncompatiblePlatformProjects); Action = MSBuild Debug; OutputPath = buildDir + "/samples" } 
 
 
@@ -128,17 +128,20 @@ Target.create "Restore" (fun _ ->
     DotNet.restore id "Fabulous.sln"
 )
 
+Target.create "BuildTools" (fun _ ->
+    tools |> buildProject
+)
+
+Target.create "RunGenerator" (fun _ ->
+    DotNet.exec id "build_output/tools/Generator/Generator.dll" "tools/Generator/Xamarin.Forms.Core.json src/Fabulous.Core/Xamarin.Forms.Core.fs" |> ignore
+)
+
 Target.create "Build" (fun _ -> 
     projects |> List.iter buildProject
 )
 
 Target.create "BuildSamples" (fun _ ->
     samples |> buildProject
-)
-
-Target.create "RunGenerator" (fun _ ->
-    DotNet.build (fun p -> { p with Configuration = DotNet.BuildConfiguration.Release }) "tools/Generator/Generator.fsproj"
-    DotNet.exec id "tools/Generator/bin/Release/netcoreapp2.0/Generator.dll" "tools/Generator/Xamarin.Forms.Core.json src/Fabulous.Core/Xamarin.Forms.Core.fs" |> ignore
 )
 
 Target.create "RunTests" (fun _ ->
@@ -196,6 +199,8 @@ open Fake.Core.TargetOperators
 "Clean"
   ==> "Restore"
   ==> "UpdateVersion"
+  ==> "BuildTools"
+  ==> "RunGenerator"
   ==> "Build"
 
 "Build"
