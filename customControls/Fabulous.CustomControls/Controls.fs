4f708e3cf7413f150b2509b4a9bbacf0c37c263b
Gareth Hubball <ridecar2@gmail.com>
2018-11-15 18:52:46 +0000

    EntryCell now uses PropertyChanged to fire TextChanged event.

---
+++ customControls/Fabulous.CustomControls/Controls.fs
@@ -0,0 +1,22 @@
+namespace Fabulous.CustomControls
+
+open Xamarin.Forms
+open System
+
+type CustomEntryCell() as self =
+    inherit EntryCell()
+
+    let mutable oldValue = ""
+    let textChanged = Event<EventHandler<TextChangedEventArgs>, TextChangedEventArgs>()
+
+    do self.PropertyChanging.Add(
+        fun args ->
+            if args.PropertyName = "Text" then
+                oldValue <- self.Text)
+
+    do self.PropertyChanged.Add(
+        fun args ->
+            if args.PropertyName = "Text" then
+                textChanged.Trigger(self, TextChangedEventArgs(oldValue, self.Text)))
+
+    [<CLIEvent>] member __.TextChanged = textChanged.Publish
