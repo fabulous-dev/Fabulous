6b3ecbcb9be86e0792be2c7cef01a20e27d5c0e1
Mark Allibone <mallibone@outlook.com>
2018-12-31 16:52:55 +0100

    * Add empty Template for GTK * Add GTK to the dotnet template

---
+++ templates/content/blank/NewApp.Gtk/Main.fs
@@ -0,0 +1,20 @@
+﻿namespace NewApp.Gtk
+    module Main =
+
+        open System
+        open Xamarin.Forms
+        open Xamarin.Forms.Platform.GTK
+
+        [<EntryPoint>]
+        let Main(args) =
+            Gtk.Application.Init()
+            Forms.Init()
+
+            let app = new NewApp.App()
+            let window = new FormsWindow()
+            window.LoadApplication(app)
+            window.SetApplicationTitle("Hello Fabulous GTK#")
+            window.Show();
+
+            Gtk.Application.Run()
+            0
