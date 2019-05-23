3bdf0c2d873f505afbd1941b06d85c8c2b7d8f76
Timothé Larivière <timothe.lariviere@outlook.com>
2019-05-23 20:08:23 +0200

    mkProgramWithCmdMsg

--- samples/CounterApp/CounterApp/CounterApp.fs
+++ samples/CounterApp/CounterApp/CounterApp.fs
@@ -1,7 +1,6 @@
 ﻿// Copyright 2018 Fabulous contributors. See LICENSE.md for license.
 namespace CounterApp
 
-open System.Diagnostics
 open Fabulous.Core
 open Fabulous.DynamicViews
 open Xamarin.Forms
@@ -23,18 +22,18 @@ module App =
     type CmdMsg =
         | TickTimer
 
-    let initModel () = { Count = 0; Step = 1; TimerOn=false }
-
-    let init () = initModel () , []
-
     let timerCmd () = 
         async { do! Async.Sleep 200
                 return TimedTick }
         |> Cmd.ofAsyncMsg
 
-    let cmdMsgToCmd cmdMsg =
+    let mapCmdMsgToCmd cmdMsg =
         match cmdMsg with
         | TickTimer -> timerCmd()
+
+    let initModel () = { Count = 0; Step = 1; TimerOn=false }
+
+    let init () = initModel () , []
 
     let update msg model =
         match msg with
@@ -48,38 +47,28 @@ module App =
     let view (model: Model) dispatch =  
         View.ContentPage(
           content=View.StackLayout(padding=30.0,verticalOptions = LayoutOptions.Center,
-            children=[ 
-              // If you want the button to disappear when in the initial condition then use this:
-              yield View.Label(automationId="CountLabel", text=sprintf "%d" model.Count, horizontalOptions=LayoutOptions.Center, widthRequest=200.0, horizontalTextAlignment=TextAlignment.Center)
-              yield View.Button(automationId="IncrementButton", text="Increment", command= (fun () -> dispatch Increment))
-              yield View.Button(automationId="DecrementButton", text="Decrement", command= (fun () -> dispatch Decrement)) 
-              yield View.StackLayout(padding=20.0, orientation=StackOrientation.Horizontal, horizontalOptions=LayoutOptions.Center,
+            children=[
+              View.Label(automationId="CountLabel", text=sprintf "%d" model.Count, horizontalOptions=LayoutOptions.Center, widthRequest=200.0, horizontalTextAlignment=TextAlignment.Center)
+              View.Button(automationId="IncrementButton", text="Increment", command= (fun () -> dispatch Increment))
+              View.Button(automationId="DecrementButton", text="Decrement", command= (fun () -> dispatch Decrement)) 
+              View.StackLayout(padding=20.0, orientation=StackOrientation.Horizontal, horizontalOptions=LayoutOptions.Center,
                               children = [ View.Label(text="Timer")
                                            View.Switch(automationId="TimerSwitch", isToggled=model.TimerOn, toggled=(fun on -> dispatch (TimerToggled on.Value))) ])
-              yield View.Slider(automationId="StepSlider", minimumMaximum=(0.0, 10.0), value= double model.Step, valueChanged=(fun args -> dispatch (SetStep (int (args.NewValue + 0.5)))))
-              yield View.Label(automationId="StepSizeLabel", text=sprintf "Step size: %d" model.Step, horizontalOptions=LayoutOptions.Center) 
-              //if model <> initModel () then 
-              yield View.Button(text="Reset", horizontalOptions=LayoutOptions.Center, command=(fun () -> dispatch Reset), canExecute = (model <> initModel () ))
-            ]))   
-
-    let convertedInit () =
-        let newModel, cmdMsgs = init ()
-        let cmd = cmdMsgs |> List.map cmdMsgToCmd |> Cmd.batch
-        newModel, cmd
-
-    let convertedUpdate msg model =
-        let newModel, cmdMsgs = update msg model
-        let cmd = cmdMsgs |> List.map cmdMsgToCmd |> Cmd.batch
-        newModel, cmd
+              View.Slider(automationId="StepSlider", minimumMaximum=(0.0, 10.0), value= double model.Step, valueChanged=(fun args -> dispatch (SetStep (int (args.NewValue + 0.5)))))
+              View.Label(automationId="StepSizeLabel", text=sprintf "Step size: %d" model.Step, horizontalOptions=LayoutOptions.Center)
+              View.Button(text="Reset", horizontalOptions=LayoutOptions.Center, command=(fun () -> dispatch Reset), canExecute = (model <> initModel () ))
+            ]))
              
     let program = 
-        Program.mkProgram convertedInit convertedUpdate view 
-        |> Program.withConsoleTrace
+        Program.mkProgramWithCmdMsg init update view mapCmdMsgToCmd
 
 type CounterApp () as app = 
     inherit Application ()
 
-    let runner = App.program |> Program.runWithDynamicView app
+    let runner =
+        App.program
+        |> Program.withConsoleTrace
+        |> Program.runWithDynamicView app
 
 #if DEBUG
     // Run LiveUpdate using: 
