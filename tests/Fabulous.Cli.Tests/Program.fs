b0dc27d6b72f988f78149051c920365bb52167d4
Timothé Larivière <timothe.lariviere@outlook.com>
2019-05-30 16:37:05 +0200

    Fix warnings

---
+++ tests/Fabulous.Cli.Tests/Program.fs
@@ -0,0 +1,7 @@
+﻿// Copyright 2018 Fabulous contributors. See LICENSE.md for license.
+namespace Fabulous.Cli.Tests
+
+module Program =
+    [<EntryPoint>]
+    let main (argv: string[]) =
+        0
