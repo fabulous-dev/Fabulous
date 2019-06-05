da5cf11a058b0b119c6a9da0e289e15abafaf5c4
LePelicaN <lepelican@gmail.com>
2019-06-05 23:01:03 +0200

    Small update from code review + example

--- extensions/VideoManager/VideoManager.fs
+++ extensions/VideoManager/VideoManager.fs
@@ -16,7 +16,7 @@ module VideoManagerExtension =
     type Fabulous.DynamicViews.View with
         /// Describes a VideoView in the view
         static member inline VideoView
-            (source: obj, ?videoAspect: VideoAspectMode , ?ShowControls: bool,
+            (?source: obj, ?videoAspect: VideoAspectMode , ?showControls: bool,
              // inherited attributes common to all views
              ?horizontalOptions, ?verticalOptions, ?margin, ?gestureRecognizers, ?anchorX, ?anchorY, ?backgroundColor, ?heightRequest,
              ?inputTransparent, ?isEnabled, ?isVisible, ?minimumHeightRequest, ?minimumWidthRequest, ?opacity,
@@ -24,10 +24,10 @@ module VideoManagerExtension =
              ?resources, ?styles, ?styleSheets, ?classId, ?styleId, ?automationId, ?created, ?styleClass) =
 
             // Count the number of additional attributes
-            let attribCount = 1 // source
-            let incIfSome attr count = match attr with Some _ -> count + 1 | None -> count
-            let attribCount = incIfSome videoAspect attribCount
-            let attribCount = incIfSome ShowControls attribCount
+            let attribCount = 0
+            let attribCount = match source with Some _ -> attribCount + 1 | None -> attribCount
+            let attribCount = match videoAspect with Some _ -> attribCount + 1 | None -> attribCount
+            let attribCount = match showControls with Some _ -> attribCount + 1 | None -> attribCount
 
             // Populate the attributes of the base element
             let attribs =
@@ -42,10 +42,9 @@ module VideoManagerExtension =
                                        ?automationId=automationId, ?created=created, ?styleClass=styleClass)
 
             // Add our own attributes.
-            attribs.Add(SourceAttribKey, source)
-            let addIfSome (attribs: AttributesBuilder) prop key = match prop with None -> () | Some v -> attribs.Add(key, v)
-            addIfSome attribs videoAspect VideoAspectAttribKey
-            addIfSome attribs ShowControls ShowControlsAttribKey
+            match source with None -> () | Some v -> attribs.Add(SourceAttribKey, v)
+            match videoAspect with None -> () | Some v -> attribs.Add(VideoAspectAttribKey, v)
+            match showControls with None -> () | Some v -> attribs.Add(ShowControlsAttribKey, v)
 
             // The create method
             let create () = new VideoView()
@@ -59,4 +58,13 @@ module VideoManagerExtension =
 
             // The element
             ViewElement.Create(create, update, attribs)
+
+#if DEBUG
+    let sample1 =
+        View.VideoView(
+            source = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+            showControls = false,
+            heightRequest = 500.,
+            widthRequest = 200.)
+#endif
 
