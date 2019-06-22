0a8e384a07dddbfb3c980c8a8ad8f4957856f653
Timothé Larivière <timothe.lariviere@outlook.com>
2019-06-22 15:08:51 +0200

    Add JDK path for samples

--- build.fsx
+++ build.fsx
@@ -62,9 +62,16 @@ let getOutputDir basePath proj =
     let folderName = Path.GetFileNameWithoutExtension(proj)
     sprintf "%s/%s/" basePath folderName
 
+// JavaSdkDirectory is a temporary fix for the Windows 2019 build agent
+// It's currently failing to build Xamarin.Android : https://github.com/Microsoft/azure-pipelines-image-generation/blob/master/images/win/Vs2019-Server2019-Readme.md
+let addJDK properties =
+    match Environment.environVarOrDefault "JAVA_HOME_8_X64" "" with
+    | javaHome when not (System.String.IsNullOrWhiteSpace javaHome && Environment.isWindows) -> ("JavaSdkDirectory", javaHome) :: properties
+    | _ -> properties
+
 let msbuild (buildType: BuildType) (definition: ProjectDefinition) =
     let configuration = match buildType with Debug -> "Debug" | Release -> "Release"
-    let properties = [ ("Configuration", configuration) ] 
+    let properties = [ ("Configuration", configuration) ] |> addJDK
 
     for project in definition.Path do
         let outputDir = getOutputDir definition.OutputPath project
@@ -201,13 +208,6 @@ Target.create "TestTemplatesNuGet" (fun _ ->
     if Environment.isWindows then
         restorePackageDotnetCli testAppName (testAppName + ".WPF") "fsproj" pkgs
         restorePackageDotnetCli testAppName (testAppName + ".UWP") "csproj" pkgs
-
-    // JavaSdkDirectory is a temporary fix for the Windows 2019 build agent
-    // It's currently failing to build Xamarin.Android : https://github.com/Microsoft/azure-pipelines-image-generation/blob/master/images/win/Vs2019-Server2019-Readme.md
-    let addJDK properties =
-        match Environment.environVarOrDefault "JAVA_HOME_8_X64" "" with
-        | javaHome when not (System.String.IsNullOrWhiteSpace javaHome && Environment.isWindows) -> ("JavaSdkDirectory", javaHome) :: properties
-        | _ -> properties
 
     // Build for all combinations
     for c in ["Debug"; "Release"] do 
