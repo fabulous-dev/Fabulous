af26d2cc64172ce0405f900051103d4db2c375f8
Timothé Larivière <timothe.lariviere@outlook.com>
2019-08-13 17:16:14 +0200

    Fixed build script not running F.XF tests projects

--- build.fsx
+++ build.fsx
@@ -187,8 +187,8 @@ Target.create "BuildFabulousXamarinForms" (fun _ ->
 )
 
 Target.create "RunFabulousXamarinFormsTests" (fun _ ->
-    !! "tests/**/*.fsproj"
-    |> dotnetTest "Fabulous/TestResults"
+    !! "Fabulous.XamarinForms/tests/**/*.fsproj"
+    |> dotnetTest "Fabulous.XamarinForms/TestResults"
 )
 
 Target.create "BuildFabulousXamarinFormsExtensions" (fun _ ->
@@ -237,7 +237,7 @@ Target.create "BuildFabulousXamarinFormsSamples" (fun _ ->
 
 Target.create "RunFabulousXamarinFormsSamplesTests" (fun _ ->
     !! "Fabulous.XamarinForms/samples/**/*.Tests.fsproj"
-    |> dotnetTest "Fabulous.XamarinForms/TestResults"
+    |> dotnetTest "Fabulous.XamarinForms/samples/TestResults"
 )
 
 Target.create "BuildFabulousStaticViewSamples" (fun _ ->
