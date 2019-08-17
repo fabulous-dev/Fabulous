0c40fb3a171a2768739593af3a1fce0c9d21dbc7
Timothé Larivière <timothe.lariviere@outlook.com>
2019-08-17 16:56:01 +0200

    Fixed models

--- src/Fabulous.CodeGen/Models.fs
+++ src/Fabulous.CodeGen/Models.fs
@@ -28,86 +28,99 @@ module Models =
     type IConstructorMember =
         /// The name of the member in the constructor, should be in a lower camel case (e.g. ItemsSource => items | View.ListView(items=...))
         /// If a Name is provided, the ShortName will be automatically determined from it
-        abstract member ShortName : string option with get, set
+        abstract ShortName : string option
 
     type IProperty =
         /// The default value of the property when none is given by the user
-        abstract member DefaultValue : string option with get, set
+        abstract DefaultValue : string option
         
         /// The type of the elements if the property is a collection
-        abstract member ElementType : string option with get, set
+        abstract ElementType : string option
         
         /// The function that converts the model value to the actual type expected by the property
-        abstract member ConvertModelToValue : string option with get, set
+        abstract ConvertModelToValue : string option
         
         /// The function that will handle the update process for this property
         /// Completely replace the code generated for this property in Update*ControlType*
-        abstract member UpdateCode : string option with get, set
+        abstract UpdateCode : string option
 
     type Property() =
         inherit Member()
+        
+        member val ShortName = None with get, set
+        member val DefaultValue = None with get, set
+        member val ElementType = None with get, set
+        member val ConvertModelToValue = None with get, set
+        member val UpdateCode = None with get, set
+        
         interface IConstructorMember with
-            member val ShortName = None with get, set
+            member x.ShortName = x.ShortName
         interface IProperty with
-            member val DefaultValue = None with get, set
-            member val ElementType = None with get, set
-            member val ConvertModelToValue = None with get, set
-            member val UpdateCode = None with get, set
+            member x.DefaultValue = x.DefaultValue
+            member x.ElementType = x.ElementType
+            member x.ConvertModelToValue = x.ConvertModelToValue
+            member x.UpdateCode = x.UpdateCode
 
     type Event() =
         inherit Member()
+        
+        member val ShortName = None with get, set
         
         /// The properties' name which are linked to this event (e.g. TextChanged => Text)
         member val RelatedProperties : string[] option = None with get, set
         
         interface IConstructorMember with
-            member val ShortName = None with get, set
+            member x.ShortName = x.ShortName
         
     type AttachedProperty() =
         inherit Member()
         
         /// The type to which this attached property will be applied to
         member val TargetType : string option = None with get, set
+        member val DefaultValue = None with get, set
+        member val ElementType = None with get, set
+        member val ConvertModelToValue = None with get, set
+        member val UpdateCode = None with get, set
         
         interface IProperty with
-            member val DefaultValue = None with get, set
-            member val ElementType = None with get, set
-            member val ConvertModelToValue = None with get, set
-            member val UpdateCode = None with get, set
+            member x.DefaultValue = x.DefaultValue
+            member x.ElementType = x.ElementType
+            member x.ConvertModelToValue = x.ConvertModelToValue
+            member x.UpdateCode = x.UpdateCode
     
     type Type() =
         /// The full name of the type to include (e.g. Xamarin.Forms.Button)
-        member val Type: string = "" with get, set
+        member val Type : string = "" with get, set
         
         /// The full name of the type to instantiate (e.g. Fabulous.XamarinForms.CustomButton)
-        member val CustomType: string option = None with get, set
+        member val CustomType : string option = None with get, set
 
         /// Indicates if this type can be instantiated by itself and if the generator should provide a public constructor for it
-        member val CanBeInstantiated: bool = true with get, set
+        member val CanBeInstantiated : bool = true with get, set
 
         /// The name of the type as used inside Fabulous (e.g. MyWonderfulButton => View.MyWonderfulButton(...))
-        member val Name: string option = None with get, set
+        member val Name : string option = None with get, set
         
         /// The events to include/create/override for this type
-        member val Events: Event[] option = None with get, set
+        member val Events : Event[] option = None with get, set
         
         /// The properties to include/create/override for this type
-        member val Properties: Property[] option = None with get, set
+        member val Properties : Property[] option = None with get, set
         
         /// The attached properties to include/create/override for this type
-        member val AttachedProperties: AttachedProperty[] option = None with get, set
+        member val AttachedProperties : AttachedProperty[] option = None with get, set
         
         /// An ordered list of all direct members (events and properties, not attached properties) to determine the order of the constructor (e.g. ["Text", "TextChanged", "Font"] => View.Entry(text=..., textChanged=..., font=...)
         /// Values must be Name
         /// Can order inherited members
-        member val ConstructorMemberOrdering: string[] option = None with get, set
+        member val ConstructorMemberOrdering : string[] option = None with get, set
     
     type OverwriteData() =
         /// Assemblies to read (can be relative paths to dlls)
-        member val Assemblies: string[] = [||] with get, set
+        member val Assemblies : string[] = [||] with get, set
         
         /// The namespace under which all the generated code will be put
-        member val OutputNamespace: string = "" with get, set
+        member val OutputNamespace : string = "" with get, set
         
         /// The types to include in the generated code
-        member val Types: Type[] = [||] with get, set
+        member val Types : Type[] = [||] with get, set
