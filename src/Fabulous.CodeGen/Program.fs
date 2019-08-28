d89c0b56bad7b74ea7bc9db7b72fb2813b61e386
Timothé Larivière <timothe.lariviere@outlook.com>
2019-08-28 23:38:43 +0200

    Workflow

--- src/Fabulous.CodeGen/Program.fs
+++ src/Fabulous.CodeGen/Program.fs
@@ -10,7 +10,7 @@ open Fabulous.CodeGen.Binder
 open Fabulous.CodeGen.Binder.Models
 open Fabulous.CodeGen.Generator
 
-type MainWorkflowConfiguration =
+type WorkflowConfiguration =
     { loadBindings: string -> WorkflowResult<Bindings>
       readAssemblies: Configuration -> ReadAssembliesConfiguration -> string array -> WorkflowResult<AssemblyType array>
       bind: AssemblyType array -> Bindings -> WorkflowResult<BoundModel>
@@ -29,7 +29,7 @@ and ReadAssembliesConfiguration =
 and Program =
     { debug: bool
       configuration: Configuration
-      mainWorkflow: MainWorkflowConfiguration
+      workflow: WorkflowConfiguration
       readAssembliesConfiguration: ReadAssembliesConfiguration }
     
 module private Functions =
@@ -62,31 +62,31 @@ module private Functions =
     
     let runProgram program bindingsFile outputFile =        
         let readAssemblies (bindings: Bindings) =
-            program.mainWorkflow.readAssemblies program.configuration program.readAssembliesConfiguration bindings.Assemblies
+            program.workflow.readAssemblies program.configuration program.readAssembliesConfiguration bindings.Assemblies
             |> WorkflowResult.debug program.debug "reader-data.json"
             |> WorkflowResult.map (fun x -> (x, bindings))
             
         let bind (readerData, bindings) =
-            program.mainWorkflow.bind readerData bindings
+            program.workflow.bind readerData bindings
             |> WorkflowResult.debug program.debug "bindings.json"
             |> WorkflowResult.map (fun x -> (x, readerData))
             
         let optimize (boundModel, readerData) =
-            program.mainWorkflow.optimize boundModel
+            program.workflow.optimize boundModel
             |> WorkflowResult.debug program.debug "optimized-bindings.json"
             |> WorkflowResult.map (fun x -> (x, readerData))
             
         let expand (boundModel, readerData) =
-            program.mainWorkflow.expand readerData boundModel
+            program.workflow.expand readerData boundModel
             |> WorkflowResult.debug program.debug "expanded-bindings.json"
             
         let generateCode boundModel =
-            program.mainWorkflow.generateCode boundModel
+            program.workflow.generateCode boundModel
             
         let write outputFile generatedCode =
             File.write generatedCode outputFile
         
-        program.mainWorkflow.loadBindings bindingsFile
+        program.workflow.loadBindings bindingsFile
         |> WorkflowResult.bind readAssemblies
         |> WorkflowResult.bind bind
         |> WorkflowResult.bind optimize
@@ -99,7 +99,7 @@ module Program =
     let mkProgram loadAllAssembliesByReflection tryGetAttachedPropertyByReflection configuration =
         { debug = false
           configuration = configuration
-          mainWorkflow =
+          workflow =
               { loadBindings = Functions.readBindingsFile
                 readAssemblies = Functions.readAssemblies
                 bind = Binder.bind
@@ -118,7 +118,7 @@ module Program =
         { program with debug = debug }
         
     let withReadBindingsFile func program =
-        { program with mainWorkflow = { program.mainWorkflow with loadBindings = func } }
+        { program with workflow = { program.workflow with loadBindings = func } }
         
     let withIsTypeResolvable func program =
         { program with readAssembliesConfiguration = { program.readAssembliesConfiguration with isTypeResolvable = func } }
