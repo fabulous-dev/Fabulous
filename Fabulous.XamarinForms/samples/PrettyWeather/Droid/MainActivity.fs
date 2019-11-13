09be0c1b65a82753f07fdb29cbb16efa781f9416
Chris Evans <Cevans@peerlessproducts.com>
2019-11-12 19:26:30 -0600

    fixed android issue caused by moving the project

--- Fabulous.XamarinForms/samples/PrettyWeather/Droid/MainActivity.fs
+++ Fabulous.XamarinForms/samples/PrettyWeather/Droid/MainActivity.fs
@@ -12,11 +12,5 @@ type MainActivity() =
     inherit FormsApplicationActivity()
     override this.OnCreate (bundle: Bundle) =
         base.OnCreate (bundle)
-        Xamarin.Essentials.Platform.Init(this, bundle)
         Xamarin.Forms.Forms.Init (this, bundle)
-
         this.LoadApplication (new PrettyWeather.PrettyWeatherApp())
-
-    override this.OnRequestPermissionsResult (requestCode,permissions,grantResults) =
-        Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        base.OnRequestPermissionsResult(requestCode, permissions, grantResults)
