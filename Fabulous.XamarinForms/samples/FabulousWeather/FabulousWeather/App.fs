5dcad9be032d358a0f21a9b0055ac507f5d579ce
Chris Evans <chrisevans9629@gmail.com>
2019-11-21 11:40:46 -0600

    improved formatting and removed unused code

--- Fabulous.XamarinForms/samples/FabulousWeather/FabulousWeather/App.fs
+++ Fabulous.XamarinForms/samples/FabulousWeather/FabulousWeather/App.fs
@@ -29,15 +29,17 @@ module AppStyles =
 
 module App =
     
-    type DataItem = { Name:string
-                      Value:obj }
-    type Model = { Temp:decimal
-                   Items: DataItem list
-                   IsRefreshing: bool
-                   Weather:string}
+    type DataItem = 
+        { Name: string
+          Value: obj }
+    type Model = 
+        { Temp: decimal
+          Items: DataItem list
+          IsRefreshing: bool
+          Weather: string }
     type Msg =
-           | RequestRefresh
-           | WeatherRefreshed of Model
+        | RequestRefresh
+        | WeatherRefreshed of Model
     let apiKey = "API KEY HERE"
 
     let kelvinToFahrenheit K = 9.0m / 5.0m * (K - 273.0m) + 32.0m
@@ -45,29 +47,31 @@ module App =
     type jsonProvider = FSharp.Data.JsonProvider<"https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22">
 
     let getOpenWeatherData apiKey = async {
-                let url = sprintf "http://api.openweathermap.org/data/2.5/weather?q=seattle&APPID=%s" apiKey
-                let! result = jsonProvider.AsyncLoad(url)
-                let model = { Temp=result.Main.Temp |> kelvinToFahrenheit
-                              Weather=(result.Weather |> Array.head).Main
-                              Items=[ 
-                                   { Name="Pressure";   Value=result.Main.Pressure }
-                                   { Name="Humidity";   Value=result.Main.Humidity }
-                                   { Name="Wind Speed"; Value=result.Wind.Speed }
-                                   { Name="Min Temp";   Value=result.Main.TempMin |> kelvinToFahrenheit }
-                                   { Name="Max Temp";   Value=result.Main.TempMax |> kelvinToFahrenheit } ]
-                              IsRefreshing = false } 
-                return WeatherRefreshed model } |> Cmd.ofAsyncMsg
+        let url = sprintf "http://api.openweathermap.org/data/2.5/weather?q=seattle&APPID=%s" apiKey
+        let! result = jsonProvider.AsyncLoad(url)
+        let model = 
+            { Temp=result.Main.Temp |> kelvinToFahrenheit
+              Weather=(result.Weather |> Array.head).Main
+              Items=[ 
+                { Name="Pressure";   Value=result.Main.Pressure }
+                { Name="Humidity";   Value=result.Main.Humidity }
+                { Name="Wind Speed"; Value=result.Wind.Speed }
+                { Name="Min Temp";   Value=result.Main.TempMin |> kelvinToFahrenheit }
+                { Name="Max Temp";   Value=result.Main.TempMax |> kelvinToFahrenheit } ]
+              IsRefreshing = false } 
+        return WeatherRefreshed model } |> Cmd.ofAsyncMsg
                 
     
-    let initModel =   { Temp=61m
-                        Weather="SUNNY"
-                        Items=[ 
-                            { Name="Pressure";   Value=10 }
-                            { Name="Humidity";   Value=65 }
-                            { Name="Wind Speed"; Value=0 }
-                            { Name="Min Temp";   Value=50 }
-                            { Name="Max Temp";   Value=80 } ] 
-                        IsRefreshing = true }
+    let initModel =
+        { Temp=61m
+          Weather="SUNNY"
+          Items=[ 
+            { Name="Pressure";   Value=10 }
+            { Name="Humidity";   Value=65 }
+            { Name="Wind Speed"; Value=0 }
+            { Name="Min Temp";   Value=50 }
+            { Name="Max Temp";   Value=80 } ] 
+          IsRefreshing = true }
     let initial() = initModel, getOpenWeatherData apiKey
 
     let update msg model =
@@ -79,14 +83,14 @@ module App =
         
         let itemsView =
             [for r in model.Items -> 
-                  View.PancakeView(content=
-                            View.Label(sprintf "%s%s%O" r.Name System.Environment.NewLine r.Value,textColor=AppStyles.MainTextColor),
-                            cornerRadius=CornerRadius(20.,20.,20.,0.),
-                            backgroundGradientStartColor=AppStyles.itemStartColor,
-                            backgroundGradientEndColor=AppStyles.itemEndColor,
-                            padding=Thickness(8.),
-                            backgroundGradientAngle=315
-                        ) ]
+                View.PancakeView(content=
+                        View.Label(sprintf "%s%s%O" r.Name System.Environment.NewLine r.Value,textColor=AppStyles.MainTextColor),
+                        cornerRadius=CornerRadius(20.,20.,20.,0.),
+                        backgroundGradientStartColor=AppStyles.itemStartColor,
+                        backgroundGradientEndColor=AppStyles.itemEndColor,
+                        padding=Thickness(8.),
+                        backgroundGradientAngle=315
+                    ) ]
 
         let grid =
             View.Grid(rowdefs=[ Auto; Star; Auto; Auto; Auto; Auto ])
@@ -115,8 +119,7 @@ module App =
                                         orientation=StackOrientation.Horizontal
                                         ).Margin(Thickness(10.)),
                                 orientation=ScrollOrientation.Horizontal
-                                    )
-                            .Row(5)
+                                    ).Row(5)
 
                     ]
                     )
@@ -148,36 +151,4 @@ type App () as app =
 
 #if DEBUG
     do runner.EnableLiveUpdate ()
-#endif
-
-
-#if SAVE_MODEL_WITH_JSON
-    let modelId = "model"
-    override __.OnSleep() = 
-
-        let json = Newtonsoft.Json.JsonConvert.SerializeObject(runner.CurrentModel)
-        Debug.WriteLine("OnSleep: saving model into app.Properties, json = {0}", json)
-
-        app.Properties.[modelId] <- json
-
-    override __.OnResume() = 
-        Debug.WriteLine "OnResume: checking for model in app.Properties"
-        try 
-            match app.Properties.TryGetValue modelId with
-            | true, (:? string as json) -> 
-
-                Debug.WriteLine("OnResume: restoring model from app.Properties, json = {0}", json)
-                let model = Newtonsoft.Json.JsonConvert.DeserializeObject<App.Model>(json)
-
-                Debug.WriteLine("OnResume: restoring model from app.Properties, model = {0}", (sprintf "%0A" model))
-                runner.SetCurrentModel (model, Cmd.none)
-
-            | _ -> ()
-        with ex -> 
-            App.program.onError("Error while restoring model found in app.Properties", ex)
-
-    override this.OnStart() = 
-        Debug.WriteLine "OnStart: using same logic as OnResume()"
-        this.OnResume()
-
 #endif
