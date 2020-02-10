55067f43d6ab867fc3cd035755fd423c6a7c85c6
Timothé Larivière <timothe.lariviere@outlook.com>
2020-02-10 23:44:06 +0100

    Fix Windows

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@
 cls
 
 REM Allow FAKE to run on .NET Core 3.x
-set DOTNET_ROLL_FORWARD=Major 
+set DOTNET_ROLL_FORWARD=Major
 
 dotnet restore build.proj
 if "%~1"=="" (dotnet fake build) else (dotnet fake run build.fsx -t %*)
