c52bcb73e402229ebc0e05037de478bc1d0ca4eb
Timothé Larivière <timothe.lariviere@outlook.com>
2020-03-26 15:39:10 +0100

    Various updates

--- Fabulous.XamarinForms/tests/FSharpDaemon.Driver.Tests/Program.fs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright 2018-2019 Fabulous contributors. See LICENSE.md for license.
-namespace FSharpDaemon.DriverTests
-
-module Program =
-    [<EntryPoint>]
-    let main (argv: string[]) =
-        0
--- Fabulous.XamarinForms/tests/Fabulous.XamarinForms.Tests/Program.fs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright 2018-2019 Fabulous contributors. See LICENSE.md for license.
-namespace Generator.Tests
-
-module Program =
-    [<EntryPoint>]
-    let main (argv: string[]) =
-        0
