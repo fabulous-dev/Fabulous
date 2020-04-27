e4eaaf7420f23c4839dcdfe0be48fecc11f916cc
Vasily Shapenko <tobasil@gmail.com>
2020-04-27 19:57:24 +0300

    Added bunch of tests

--- Fabulous.XamarinForms/tests/Fabulous.XamarinForms.Tests/ViewHelpersTests.fs
+++ Fabulous.XamarinForms/tests/Fabulous.XamarinForms.Tests/ViewHelpersTests.fs
@@ -1,6 +1,8 @@
 ﻿// Copyright 2018-2019 Fabulous contributors. See LICENSE.md for license.
 namespace Fabulous.Core.Tests
 
+open System.Collections.Generic
+open Fabulous
 open NUnit.Framework
 open FsUnit
 open Fabulous.XamarinForms
@@ -137,3 +139,230 @@ module ViewHelpersTests =
             |> findViewElement "LabelTest"
             |> ignore)
         |> should throw typeof<System.Exception>
+        
+    [<Test>]
+    let ``Given a complex layout, we should not get errors when there are no keyed controls`` () =
+        
+        let previous =
+            [|
+                View.Label(text = "Label 1")
+                View.Label(text = "Label 2")
+            |]
+
+        let current = 
+            [|
+                View.Label(text = "Label 1")
+                View.Label(text = "Label 2")
+            |]
+
+        let mutable mockUpdateCallCount = 0
+        let mockUpdate _ _  _ =
+            mockUpdateCallCount <- mockUpdateCallCount + 1
+
+        let mockAttach  _ _ _ =()
+        let mockCanReuse _ _ = false
+        let mockGetKey = ViewHelpers.getKey
+        let mutable createCount = 0
+        let mockCreate _ =  
+            createCount <- createCount + 1
+           
+        let initial = System.Collections.Generic.List<unit>() :> IList<unit>
+       
+        do updateCollectionGeneric (ValueSome previous) (ValueSome current)  initial mockCreate mockAttach mockCanReuse mockGetKey mockUpdate 
+
+        mockUpdateCallCount |> should equal 0
+        createCount |> should equal 2
+        
+    [<Test>]
+    let ``Given a complex layout with keyed controls , we should create only not keyed controls`` () =
+        
+        let previous =
+            [|
+                View.Label(key="Key_1",text = "Label 1")
+                View.Label(text = "Label 2")
+            |]
+
+        let current = 
+            [|
+                View.Entry(text = "Entry 3")
+                View.Label(key="Key_1",text = "Label 1")
+                View.Label(text = "Label 2")
+            |]
+
+        let mutable mockUpdateCallCount = 0
+        let mockUpdate _ _  _ =
+            mockUpdateCallCount <- mockUpdateCallCount + 1
+
+        let mockAttach  _ _ _ =()
+        let mockCanReuse _ _ = false
+        let mockGetKey = ViewHelpers.getKey
+        let initial = System.Collections.Generic.List<unit>() :> IList<unit>
+        initial.Add ()
+        initial.Add ()
+        let mutable createCount = 0
+        let mockCreate _ =
+            initial.Add (())
+            createCount <- createCount + 1
+           
+        
+       
+        do updateCollectionGeneric (ValueSome previous) (ValueSome current)  initial mockCreate mockAttach mockCanReuse mockGetKey mockUpdate 
+
+        createCount |> should equal 2
+        
+        
+    [<Test>]
+    let ``Adding new control at the end of list invokes create`` () =
+        
+        let previous =
+            [|
+                View.Label(text = "Label 1")
+                View.Entry(text = "Label 2")
+            |]
+
+        let current = 
+            [|
+                
+                View.Label(text = "Label 1")
+                View.Entry(text = "Label 2")
+                View.Grid()
+            |]
+
+        let mutable mockUpdateCallCount = 0
+        let mockUpdate _ _  _ =
+            mockUpdateCallCount <- mockUpdateCallCount + 1
+
+        let mockAttach  _ _ _ =()
+        let mockCanReuse _ _ = false
+        let mockGetKey = ViewHelpers.getKey
+        let initial = System.Collections.Generic.List<unit>() :> IList<unit>
+        initial.Add ()
+        initial.Add ()
+        let mutable createCount = 0
+        let mockCreate _ =
+            initial.Add (())
+            createCount <- createCount + 1
+           
+        
+       
+        do updateCollectionGeneric (ValueSome previous) (ValueSome current)  initial mockCreate mockAttach mockCanReuse mockGetKey mockUpdate 
+
+        createCount |> should equal 3
+        
+        
+    [<Test>]
+    let ``Removing  control from the start breaks all`` () =
+        
+        let previous =
+            [|
+                View.Grid() 
+                View.Label(text = "Label 1")
+                View.Entry(text = "Label 2")
+            |]
+
+        let current = 
+            [|
+                View.Label(text = "Label 1")
+                View.Entry(text = "Label 2")
+               
+            |]
+
+        let mutable mockUpdateCallCount = 0
+        let mockUpdate _ _  _ =
+            mockUpdateCallCount <- mockUpdateCallCount + 1
+
+        let mockAttach  _ _ _ =()
+        let mockCanReuse _ _ = false
+        let mockGetKey = ViewHelpers.getKey
+        let initial = System.Collections.Generic.List<ViewElement>() :> IList<ViewElement>
+        let mutable createCount = 0
+        let mockCreate v =
+
+            createCount <- createCount + 1
+            v
+           
+        
+       
+        do updateCollectionGeneric (ValueSome previous) (ValueSome current)  initial mockCreate mockAttach mockCanReuse mockGetKey mockUpdate 
+
+        createCount|>should equal 3
+
+    [<Test>]
+    let ``Removing  control from the end looks ok `` () =
+        
+        let previous =
+            [|
+               
+                View.Label(text = "Label 1")
+                View.Entry(text = "Label 2")
+                View.Grid() 
+            |]
+
+        let current = 
+            [|
+                View.Label(text = "Label 1")
+                View.Entry(text = "Label 2")
+                
+               
+            |]
+
+        let mutable mockUpdateCallCount = 0
+        let mockUpdate _ _  _ =
+            mockUpdateCallCount <- mockUpdateCallCount + 1
+
+        let mockAttach  _ _ _ =()
+        let mockCanReuse _ _ = false
+        let mockGetKey = ViewHelpers.getKey
+        let initial = System.Collections.Generic.List<ViewElement>() :> IList<ViewElement>
+        let mutable createCount = 0
+        let mockCreate v =
+            createCount <- createCount + 1
+            v
+           
+        
+       
+        do updateCollectionGeneric (ValueSome previous) (ValueSome current)  initial mockCreate mockAttach mockCanReuse mockGetKey mockUpdate 
+
+        createCount|>should equal 2
+        
+    [<Test>]
+    let ``Moving control with key to the end should be ok `` () =
+        
+        let previous =
+            [|
+               
+                View.Grid(key ="Key") 
+                View.Label(text = "Label 1")
+                View.Entry(text = "Label 2")
+            |]
+
+        let current = 
+            [|
+                View.Label(text = "Label 1")
+                View.Entry(text = "Label 2")
+                View.Grid(key = "Key") 
+               
+            |]
+
+        let mutable mockUpdateCallCount = 0
+        let mockUpdate _ _  _ =
+            mockUpdateCallCount <- mockUpdateCallCount + 1
+
+        let mockAttach  _ _ _ =()
+        let mockCanReuse _ _ = false
+        let mockGetKey = ViewHelpers.getKey
+        let initial = System.Collections.Generic.List<ViewElement>() :> IList<ViewElement>
+        for v in previous do
+            initial.Add v
+        let mutable createCount = 0
+        let mockCreate v =
+            createCount <- createCount + 1
+            v
+            
+        do updateCollectionGeneric (ValueSome previous) (ValueSome current)  initial mockCreate mockAttach mockCanReuse mockGetKey mockUpdate 
+
+        createCount|>should equal 2
+    
+        
+        
+        
