95be5ed595b49b7b20929773593dd53e53cc2762
Timothé Larivière <timothe.lariviere@outlook.com>
2020-05-05 20:07:14 +0200

    Fix

--- build.fsx
+++ build.fsx
@@ -338,8 +338,8 @@ Target.create "PublishNuGetPackages" (fun _ ->
         | s when not (System.String.IsNullOrWhiteSpace s) -> s
         | _ -> failwith "Please set the NUGET_APIKEY environment variable to a NuGet API key with write access to the Fabulous packages."
 
+    // dotnet publishes both nupkg and snupkg by default
     DotNet.exec id "nuget" (sprintf "push %s/*.nupkg -k %s --skip-duplicate -s https://api.nuget.org/v3/index.json" buildDir nugetApiKey) |> ignore
-    DotNet.exec id "nuget" (sprintf "push %s/*.snupkg -k %s --skip-duplicate -s https://api.nuget.org/v3/index.json" buildDir nugetApiKey) |> ignore
 )
 
 Target.create "Prepare" ignore
