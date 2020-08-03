a3f95c8976513eee6fa8c789a1ae748246434ae4
Timothé Larivière <timothe.lariviere@outlook.com>
2020-08-03 18:59:15 +0200

    Add ShapeDemos sample

---
+++ Fabulous.XamarinForms/samples/ShapesDemo/UWP/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.Foundation;
+using Windows.UI.ViewManagement;
+
+namespace ShapesDemo.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadApplication(new ShapesDemo.App());
+        }
+    }
+}
