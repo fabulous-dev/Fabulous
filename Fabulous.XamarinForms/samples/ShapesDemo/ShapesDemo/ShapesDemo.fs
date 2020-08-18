14d3922a8e94aab83b3ebacb7d03c76d6410a8c4
Timothé Larivière <timothe.lariviere@outlook.com>
2020-08-18 13:50:44 +0200

    More samples

--- Fabulous.XamarinForms/samples/ShapesDemo/ShapesDemo/ShapesDemo.fs
+++ Fabulous.XamarinForms/samples/ShapesDemo/ShapesDemo/ShapesDemo.fs
@@ -7,19 +7,19 @@ open Fabulous.XamarinForms
 open Xamarin.Forms
 
 module App =
-    
+
     type Model = { Test: string }
     type Msg = Nope
 
     // Note, this declaration is needed if you enable LiveUpdate
-    let program = XamarinFormsProgram.mkSimple (fun () -> { Test = "" }) (fun (msg: Msg) model -> model) (fun _ _ -> PathGeometryDemoPage.view())
+    let program = XamarinFormsProgram.mkSimple (fun () -> { Test = "" }) (fun (msg: Msg) model -> model) (fun _ _ -> MonekyFaceDemoPage.view())
 
-type App () as app = 
+type App () as app =
     inherit Application ()
-    
-    do Device.SetFlags([| "Shapes_Experimental"; "AppTheme_Experimental" |]);
 
-    let runner = 
+    do Device.SetFlags([| "Shapes_Experimental"; "AppTheme_Experimental"; "Brush_Experimental" |]);
+
+    let runner =
         App.program
 #if DEBUG
         |> Program.withConsoleTrace
