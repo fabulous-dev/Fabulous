a38b2a171e300a45d62d20d122d96cd0451da592
Sergej Dick <sergej.dick@crowspace.de>
2020-09-18 14:45:27 +0200

    update doc liveupdate

--- Fabulous.XamarinForms/src/Fabulous.XamarinForms.LiveUpdate/LiveUpdate.fs
+++ Fabulous.XamarinForms/src/Fabulous.XamarinForms.LiveUpdate/LiveUpdate.fs
@@ -19,28 +19,28 @@
                     if Device.RuntimePlatform = Device.iOS then
                         printfn "  LiveUpdate: Connect using:"
                         for iip in iips do
-                            printfn "      fabulous --watch --webhook:http://%s:%d/update" iip.Address httpPort
+                            printfn "      dotnet fabulous --watch --webhook:http://%s:%d/update" iip.Address httpPort
                     elif Device.RuntimePlatform = Device.Android then
                         printfn "  LiveUpdate: On USB connect using:"
                         printfn "      adb -d forward  tcp:%d tcp:%d" httpPort httpPort
                         if httpPort = Ports.DefaultPort then
-                            printfn "      fabulous --watch --send"
+                            printfn "      dotnet fabulous --watch --send"
                         else
-                            printfn "      fabulous --watch --webhook:http://localhost:%d/update" httpPort
+                            printfn "      dotnet fabulous --watch --webhook:http://localhost:%d/update" httpPort
                             printfn "  "
                             printfn "  LiveUpdate: On Emulator connect using:"
                             printfn "      adb -e forward  tcp:%d tcp:%d" httpPort httpPort
                         if httpPort = Ports.DefaultPort then
-                            printfn "      fabulous --watch --send"
+                            printfn "      dotnet fabulous --watch --send"
                         else
-                            printfn "      fabulous --watch --webhook:http://localhost:%d/update" httpPort
+                            printfn "      dotnet fabulous --watch --webhook:http://localhost:%d/update" httpPort
                     else
                         printfn "  LiveUpdate: %s is not officially supported" Device.RuntimePlatform 
                         printfn "  LiveUpdate: You can still try to connect using:" 
                         if httpPort = Ports.DefaultPort then
-                            printfn "      fabulous --watch --send"
+                            printfn "      dotnet fabulous --watch --send"
                         else
-                            printfn "      fabulous --watch --webhook:http://localhost:%d/update" httpPort
+                            printfn "      dotnet fabulous --watch --webhook:http://localhost:%d/update" httpPort
 
                  Extensions.enableLiveUpdate printAddresses syncChangeProgram runner
                  
