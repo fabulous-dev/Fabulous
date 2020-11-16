e5486a5f07715a346a581e028a423eb66ed87093
Timothé Larivière <timothe.lariviere@outlook.com>
2020-11-16 09:08:42 +0100

    Upgrade FabulousWeather sample to Android 11

---
+++ Fabulous.XamarinForms/samples/FabulousWeather/Droid/AssemblyInfo.fs
@@ -0,0 +1,24 @@
+﻿namespace FabulousWeather.Droid
+open System.Reflection
+open System.Runtime.CompilerServices
+
+// the name of the type here needs to match the name inside the ResourceDesigner attribute
+type Resources = FabulousWeather.Droid.Resource
+[<assembly: Android.Runtime.ResourceDesigner("FabulousWeather.Droid.Resources", IsApplication=true)>]
+
+[<assembly: AssemblyTitle("Droid")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ Fabulous.XamarinForms/samples/FabulousWeather/Droid/MainActivity.fs
@@ -0,0 +1,20 @@
+﻿// Copyright Fabulous contributors. See LICENSE.md for license.
+namespace FabulousWeather.Droid
+
+open Android.App
+open Android.Content.PM
+open Android.OS
+open Xamarin.Forms.Platform.Android
+open FabulousWeather
+
+[<Activity (Label = "FabulousWeather.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = (ConfigChanges.ScreenSize ||| ConfigChanges.Orientation))>]
+type MainActivity() =
+    inherit FormsApplicationActivity()
+    override this.OnCreate (bundle: Bundle) =
+        FormsAppCompatActivity.TabLayoutResource <- Resources.Layout.Tabbar
+        FormsAppCompatActivity.ToolbarResource <- Resources.Layout.Toolbar
+        
+        base.OnCreate(bundle)
+        Xamarin.Forms.Forms.Init(this, bundle)
+        this.LoadApplication(App())
+
