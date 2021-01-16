f55ca702d2bf6bce5b5f5cebd7ae184bd210ad2e
Nikolay <dolphinn@bk.ru>
2021-01-16 17:38:32 +0300

    Fix exception

--- Fabulous.CodeGen/src/Fabulous.CodeGen/AssemblyReader/Reader.fs
+++ Fabulous.CodeGen/src/Fabulous.CodeGen/AssemblyReader/Reader.fs
@@ -112,7 +112,7 @@ module Reader =
         { FullName = tdef.FullName
           AssemblyName = tdef.Module.Assembly.Name.Name
           CanBeInstantiated = not tdef.IsAbstract && ctor.IsSome
-          HasParameterlessConstructor = ctor.Value.Parameters.Count = 0
+          HasParameterlessConstructor = ctor.IsSome && ctor.Value.Parameters.Count = 0
           InheritanceHierarchy = Resolver.getHierarchyForType baseTypeName tdef 
           Events = readEventsFromType convertTypeName tdef
           AttachedProperties = readAttachedPropertiesFromType convertTypeName tryGetStringRepresentationOfDefaultValue tryGetProperty propertyBaseType tdef
