4e80a6f6b0975c7548c10ddd2fc64c74a32d8971
Nikolay Matyushin <dolphinn@bk.ru>
2021-02-02 16:50:56 +0300

    Remove HasParameterlessConstructor and simplify CanBeInstantiated condition

--- Fabulous.CodeGen/src/Fabulous.CodeGen/AssemblyReader/Models.fs
+++ Fabulous.CodeGen/src/Fabulous.CodeGen/AssemblyReader/Models.fs
@@ -49,9 +49,6 @@ module Models =
           /// Indicates if the type can be instantiated (non-abstract type)
           CanBeInstantiated: bool
           
-          /// Indicates if the type has a public parameter-less constructor
-          HasParameterlessConstructor: bool
-          
           /// The types inherited by this type
           /// From first to last, the closest inherited type to the base
           InheritanceHierarchy: string array
