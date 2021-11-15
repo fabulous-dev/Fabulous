a115acca0628a12d0021dfe8123404faf5a95c6b
SergejDK <sergej.dick@plesian.com>
2021-11-15 13:41:17 +0100

    update everythign before running build

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 # Allow FAKE to run on .NET Core 3.x
 export DOTNET_ROLL_FORWARD=Major
 
+sudo apt-get update
+sudo apt-get install fsharp
 dotnet tool restore
 if [ $# -eq 0 ]; then
     dotnet fake build
