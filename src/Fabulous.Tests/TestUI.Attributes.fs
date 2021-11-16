889b083a07577bda8881bf3af7032fa9a7e1881e
Simon Korzunov <twop@hey.com>
2021-11-15 23:43:00 -0800

    Added "Pressed" attribute

--- src/Fabulous.Tests/TestUI.Attributes.fs
+++ src/Fabulous.Tests/TestUI.Attributes.fs
@@ -1,121 +1,58 @@
 module Fabulous.Tests.TestUI_Attributes
 
 open Fabulous
-open System
+open Test.Platform
 
 module Attributes =
-    /// Define an attribute storing a Widget for a bindable property
-    let defineBindableWidget (bindableProperty: BindableProperty) =
-        Attributes.defineWidget
-            ViewNode.getViewNode
-            bindableProperty.PropertyName
-            (fun parent ->
-                let p = (parent :?> BindableObject)
-                let x = p.GetValue(bindableProperty)
-                x
-            )
-            (fun target value ->
-                let bindableObject = target :?> BindableObject
-                if value = null then
-                    bindableObject.ClearValue(bindableProperty)
-                else
-                    bindableObject.SetValue(bindableProperty, value)
-            )
 
-    let defineBindableWithComparer<'inputType, 'modelType> (bindableProperty: Xamarin.Forms.BindableProperty) (convert: 'inputType -> 'modelType) (compare: ('modelType * 'modelType) -> ScalarAttributeComparison) =
-        Attributes.defineScalarWithConverter<'inputType, 'modelType>
-            bindableProperty.PropertyName
-            (fun () -> Unchecked.defaultof<'modelType>)
-            convert
-            compare
-            (fun (newValueOpt, target) ->
-                match newValueOpt with
-                | ValueNone -> (target :?> BindableObject).ClearValue(bindableProperty)
-                | ValueSome v -> (target :?> BindableObject).SetValue(bindableProperty, v)
-            )
-
-    let inline defineBindable<'T when 'T: equality> bindableProperty =
-        defineBindableWithComparer<'T, 'T> bindableProperty id ScalarAttributeComparers.equalityCompare
-
-    let defineEventNoArg name (getEvent: obj -> IEvent<EventHandler, EventArgs>) =
+    let definePressable name =
         let key = AttributeDefinitionStore.getNextKey()
-        let definition : ScalarAttributeDefinition<_,_> =
-            { Key = key
-              Name = name
-              DefaultWith = fun () -> null
-              Convert = id
-              Compare = ScalarAttributeComparers.noCompare
-              UpdateTarget = fun (newValueOpt, target) ->
-                let event = getEvent target
-                let viewNodeData = (target :?> Xamarin.Forms.BindableObject).GetValue(ViewNode.ViewNodeProperty) :?> ViewNodeData
 
-                match viewNodeData.TryGetHandler(key) with
-                | None -> ()
-                | Some handler -> event.RemoveHandler handler
+        let definition: ScalarAttributeDefinition<_, _> =
+            {
+                Key = key
+                Name = name
+                DefaultWith = fun () -> null
+                Convert = id
+                Compare = ScalarAttributeComparers.noCompare
+                UpdateTarget =
+                    fun (newValueOpt, target) ->
 
-                match newValueOpt with
-                | ValueNone ->
-                    viewNodeData.SetHandler(key, ValueNone)
+                        let viewNodeData =
+                            (target :?> TestViewElement)
+                                .PropertyBag.Item TestUI_ViewNode.ViewNode.ViewNodeProperty
+                            :?> TestUI_ViewNode.ViewNodeData
 
-                | ValueSome msg ->
-                    let handler = EventHandler(fun _ _ ->
-                        viewNodeData.ViewNode.Context.Dispatch msg
-                    )
-                    event.AddHandler handler
-                    viewNodeData.SetHandler(key, ValueSome handler) }
-        AttributeDefinitionStore.set key definition
-        definition
+                        let btn = target :?> IButton
 
-    let defineEvent<'args> name (getEvent: obj -> IEvent<EventHandler<'args>, 'args>) =
-        let key = AttributeDefinitionStore.getNextKey()
-        let definition : ScalarAttributeDefinition<_,_> =
-            { Key = key
-              Name = name
-              DefaultWith = fun () -> fun _ -> null
-              Convert = id
-              Compare = ScalarAttributeComparers.noCompare
-              UpdateTarget = fun (newValueOpt: ('args -> obj) voption, target) ->
+                        match viewNodeData.TryGetHandler<int>(key) with
+                        | None -> ()
+                        | Some handlerId -> btn.RemovePressListener handlerId
 
-                let event = getEvent target
-                let viewNodeData = (target :?> Xamarin.Forms.BindableObject).GetValue(ViewNode.ViewNodeProperty) :?> ViewNodeData
+                        match newValueOpt with
+                        | ValueNone -> viewNodeData.SetHandler(key, ValueNone)
 
-                match viewNodeData.TryGetHandler(key) with
-                | None ->
-                    printfn $"No old handler for {name}"
-                | Some handler ->
-                    printfn $"Removed old handler for {name}"
-                    event.RemoveHandler handler
+                        | ValueSome msg ->
+                            let handler () =
+                                viewNodeData.ViewNode.Context.Dispatch msg
 
-                match newValueOpt with
-                | ValueNone ->
-                    viewNodeData.SetHandler(key, ValueNone)
-
-                | ValueSome fn ->
-                    let handler = EventHandler<'args>(fun sender args ->
-                        printfn $"Handler for {name} triggered"
-                        let viewNodeData = (sender :?> Xamarin.Forms.BindableObject).GetValue(ViewNode.ViewNodeProperty) :?> ViewNodeData
-                        let r = fn args
-                        viewNodeData.ViewNode.Context.Dispatch r
-                    )
-                    viewNodeData.SetHandler(key, ValueSome handler)
-                    event.AddHandler handler
-                    printfn $"Added new handler for {name}"
+                            let handlerId = btn.AddPressListener handler
+                            viewNodeData.SetHandler<int>(key, ValueSome handlerId)
             }
+
         AttributeDefinitionStore.set key definition
         definition
-
-
-// --------------- Actual Properties ---------------
-module Attrs =
+    
+    // --------------- Actual Properties ---------------
     //    open Fabulous.Attributes
 
     module Text =
         let Text =
-            Attributes.define<string> "Text" (justValue "")
+            Attributes.define<string> "Text" (fun () -> "") TestUI_ViewUpdaters.updateText
 
     module TextStyle =
         let TextColor =
-            Attributes.define<string> "TextColor" (justValue "")
+            Attributes.define<string> "TextColor" (fun () -> "") TestUI_ViewUpdaters.updateTextColor
 
     module Container =
         let Children =
@@ -126,14 +63,9 @@ module Attrs =
                 Attributes.AttributeComparers.alwaysDifferent
 
     module Button =
-        let Clicked =
-            // TODO what is the proper way to represent that?
-            Attributes.define<obj option> "_Clicked" (justValue None) // TODO fix option
+        let Pressed = definePressable "Button_Pressed"
 
 
     module Automation =
         let AutomationId =
-            Attributes.define<string> "AutomationId" (justValue "")
-
-
-
+            Attributes.define<string> "AutomationId" (fun () -> "") TestUI_ViewUpdaters.updateAutomationId
---
+++ src/Fabulous.Tests/TestUI.ViewUpdaters.fs
@@ -0,0 +1,48 @@
+module Fabulous.Tests.TestUI_ViewUpdaters
+
+open Test.Platform
+
+//let applyDiffNavigationPagePages (diffs: WidgetCollectionItemChange[], target: obj) =
+//        let viewNode = ViewNode.getViewNode target :?> ViewNode
+//        let navigationPage = target :?> NavigationPage
+//        let pages = List.ofSeq navigationPage.Pages
+//
+//        for diff in diffs do
+//            match diff with
+//            | WidgetCollectionItemChange.Insert (index, widget) -> failwith "not implemented"
+//
+//            | WidgetCollectionItemChange.Update (index, diff) ->
+//                let targetItem = pages.[index]
+//                let viewNode = ViewNode.getViewNode targetItem
+//                if diff.ScalarChanges.Length > 0 then viewNode.ApplyScalarDiff(diff.ScalarChanges)
+//                if diff.WidgetChanges.Length > 0 then viewNode.ApplyWidgetDiff(diff.WidgetChanges)
+//                if diff.WidgetCollectionChanges.Length > 0 then viewNode.ApplyWidgetCollectionDiff(diff.WidgetCollectionChanges)
+//
+//            | WidgetCollectionItemChange.Replace (index, widget) -> failwith "not implemented"
+//
+//            | _ -> ()
+//
+//let updateNavigationPagePages (newValueOpt: Widget[] voption, target: obj) =
+//    let navigationPage = target :?> NavigationPage
+//    navigationPage.PopToRootAsync(false) |> ignore
+//
+//    match newValueOpt with
+//    | ValueNone -> ()
+//    | ValueSome widgets ->
+//        let viewNode = ViewNode.getViewNode target :?> ViewNode
+//        for widget in widgets do
+//            let page = Helpers.createViewForWidget viewNode.Context widget :?> Page
+//            navigationPage.PushAsync(page) |> ignore
+
+
+let updateText (newValueOpt: string voption, target: obj) = 
+    let textElement = target :?> IText
+    textElement.Text <- ValueOption.defaultValue "" newValueOpt
+    
+let updateTextColor (newValueOpt: string voption, target: obj) = 
+    let textElement = target :?> IText
+    textElement.TextColor <- ValueOption.defaultValue "" newValueOpt
+    
+let updateAutomationId (newValueOpt: string voption, target: obj) = 
+    let el = target :?> TestViewElement
+    el.AutomationId <- ValueOption.defaultValue "" newValueOpt
