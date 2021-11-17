1fc119219af0689d3101bd0ef8ef24c17a6319ac
SergejDK <sergej.dick@plesian.com>
2021-11-17 21:24:16 +0100

    try removing env

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Allow FAKE to run on .NET Core 3.x
-export DOTNET_ROLL_FORWARD=Major
+#export DOTNET_ROLL_FORWARD=Major
 
-sudo apt-get update
-sudo apt-get install fsharp
 dotnet tool restore
 if [ $# -eq 0 ]; then
     dotnet fake build
