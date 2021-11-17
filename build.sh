4d961ddefd79f22c618160c66ff28e1a2d647d5c
SergejDK <sergej.dick@plesian.com>
2021-11-17 22:23:49 +0100

    easier pipeline run for testing

--- build.sh
+++ build.sh
@@ -3,9 +3,11 @@
 # Allow FAKE to run on .NET Core 3.x
 #export DOTNET_ROLL_FORWARD=Major
 
-dotnet tool restore
-if [ $# -eq 0 ]; then
-    dotnet fake build
-else
-    dotnet fake run build.fsx -t $@
-fi
+# dotnet tool restore
+# if [ $# -eq 0 ]; then
+#     dotnet fake build
+# else
+#     dotnet fake run build.fsx -t $@
+# fi
+
+echo fsharpc --version
