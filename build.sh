74274f3fb3462b7803c021d68705492d20a61f27
SergejDK <sergej.dick@plesian.com>
2021-11-22 11:54:54 +0100

    test

--- build.sh
+++ build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 # Allow FAKE to run on .NET Core 3.x
-export DOTNET_ROLL_FORWARD=Major
+# export DOTNET_ROLL_FORWARD=Major
 
-dotnet tool restore
-if [ $# -eq 0 ]; then
-    dotnet fake build
-else
-    dotnet fake run build.fsx -t $@
-fi
+# dotnet tool restore
+# if [ $# -eq 0 ]; then
+#     dotnet fake build
+# else
+#     dotnet fake run build.fsx -t $@
+# fi
 
+dotnet msbuild Fabulous.XamarinForms/samples/AllControls/Gtk/AllControls.Gtk.fsproj -target:Build
+
+dotnet build Fabulous.XamarinForms/samples/AllControls/Gtk/AllControls.Gtk.fsproj -target:Build
+
+dotnet build Fabulous.XamarinForms/samples/AllControls/Gtk/AllControls.Gtk.fsproj -target:Rebuild
 # ls /usr/lib/mono/xbuild/Microsoft/VisualStudio/v16.0/FSharp/
 # ls /usr/lib/mono/xbuild/Microsoft/VisualStudio/v15.0/FSharp/
 # ls /usr/lib/mono/xbuild/Microsoft/VisualStudio/v14.0/FSharp/
