f835d2fa4502eceea79ef6c45be0fbc62ea572a6
Edgar <edgar.gonzalez@fundourselves.com>
2022-01-14 08:48:57 +0100

    Fix for FabulousWeather Droid

--- samples/Xamarin.Forms/FabulousWeather/FabulousWeather.Android/MainActivity.fs
+++ samples/Xamarin.Forms/FabulousWeather/FabulousWeather.Android/MainActivity.fs
@@ -1,27 +1,33 @@
-﻿namespace Fabulous.XamarinForms.Samples.FabulousWeather.Android
+﻿// Copyright 2018 Fabulous contributors. See LICENSE.md for license.
+namespace FabulousWeather.Android
+
+open System
 
 open Android.App
+open Android.Content
+open Android.Content.PM
 open Android.Runtime
-open Xamarin.Essentials
-open Xamarin.Forms
-open Xamarin.Forms.Platform.Android
+open Android.Views
+open Android.Widget
+open Android.OS
+open Fabulous.XamarinForms
 open Fabulous.XamarinForms.Samples.FabulousWeather
+open Xamarin.Forms.Platform.Android
+open FabulousWeather
 
-[<Activity(Label = "FabulousWeather.Android", MainLauncher = true, Icon = "@mipmap/icon")>]
+[<Activity (Label = "FabulousWeather.Android", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = (ConfigChanges.ScreenSize ||| ConfigChanges.Orientation))>]
 type MainActivity() =
     inherit FormsAppCompatActivity()
+    override this.OnCreate(bundle: Bundle) =
+        FormsAppCompatActivity.TabLayoutResource <- Resources.Layout.Tabbar
+        FormsAppCompatActivity.ToolbarResource <- Resources.Layout.Toolbar
 
-    override this.OnCreate(bundle) =
-        base.OnCreate(bundle)
-        Platform.Init(this, bundle)
-        Forms.Init(this, bundle)
-        this.LoadApplication(App())
+        base.OnCreate (bundle)
+        Xamarin.Essentials.Platform.Init(this, bundle)
+        Xamarin.Forms.Forms.Init(this, bundle)
+        let application : Xamarin.Forms.Application = unbox (Program.create App.program ())
+        this.LoadApplication(application)
 
-    override this.OnRequestPermissionsResult
-        (
-            requestCode: int,
-            permissions: string [],
-            [<GeneratedEnum>] grantResults: Android.Content.PM.Permission []
-        ) =
-        Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults)
+    override this.OnRequestPermissionsResult(requestCode: int, permissions: string[], [<GeneratedEnum>] grantResults: Android.Content.PM.Permission[]) =
+        Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults)
         base.OnRequestPermissionsResult(requestCode, permissions, grantResults)
