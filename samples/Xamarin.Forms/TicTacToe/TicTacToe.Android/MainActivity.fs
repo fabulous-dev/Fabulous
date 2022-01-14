0b91c991105cd20d3f80da18076645cbddadcb62
Edgar <edgar.gonzalez@fundourselves.com>
2022-01-14 09:20:50 +0100

    Fix2  for TicTacToe Droid

--- samples/Xamarin.Forms/TicTacToe/TicTacToe.Android/MainActivity.fs
+++ samples/Xamarin.Forms/TicTacToe/TicTacToe.Android/MainActivity.fs
@@ -10,21 +10,35 @@ open Android.Runtime
 open Android.Views
 open Android.Widget
 open Android.OS
+open Fabulous.XamarinForms
+open Fabulous.XamarinForms.Samples.TicTacToe
 open Xamarin.Forms.Platform.Android
 open TicTacToe
 
-[<Activity (Label = "TicTacToe.Android", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = (ConfigChanges.ScreenSize ||| ConfigChanges.Orientation))>]
+[<Activity(Label = "TicTacToe.Android",
+           Icon = "@drawable/icon",
+           Theme = "@style/MainTheme",
+           MainLauncher = true,
+           ConfigurationChanges = (ConfigChanges.ScreenSize
+                                   ||| ConfigChanges.Orientation))>]
 type MainActivity() =
     inherit FormsAppCompatActivity()
+
     override this.OnCreate(bundle: Bundle) =
         FormsAppCompatActivity.TabLayoutResource <- Resources.Layout.Tabbar
         FormsAppCompatActivity.ToolbarResource <- Resources.Layout.Toolbar
 
-        base.OnCreate (bundle)
+        base.OnCreate(bundle)
         Xamarin.Essentials.Platform.Init(this, bundle)
         Xamarin.Forms.Forms.Init(this, bundle)
-        this.LoadApplication(App())
+        let application: Xamarin.Forms.Application = unbox (Program.create App.program ())
+        this.LoadApplication(application)
 
-    override this.OnRequestPermissionsResult(requestCode: int, permissions: string[], [<GeneratedEnum>] grantResults: Android.Content.PM.Permission[]) =
+    override this.OnRequestPermissionsResult
+        (
+            requestCode: int,
+            permissions: string [],
+            [<GeneratedEnum>] grantResults: Android.Content.PM.Permission []
+        ) =
         Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults)
         base.OnRequestPermissionsResult(requestCode, permissions, grantResults)
