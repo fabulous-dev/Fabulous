eff9867c0f0bf081a93f3d8efb54bba8c31d3148
Edgar <edgar.gonzalez@fundourselves.com>
2022-02-09 13:13:20 +0100

    Entry extensions

--- src/Fabulous.XamarinForms/Views/Controls/_InputView.fs
+++ src/Fabulous.XamarinForms/Views/Controls/_InputView.fs
@@ -1,5 +1,6 @@
 namespace Fabulous.XamarinForms
 
+open System.Runtime.CompilerServices
 open Fabulous
 open Xamarin.Forms
 
@@ -7,7 +8,49 @@ type IInputView =
     inherit IView
 
 module InputView =
+
+    let CharacterSpacing =
+        Attributes.defineBindable<float> InputView.CharacterSpacingProperty
+
+    let Placeholder =
+        Attributes.defineBindable<string> InputView.PlaceholderProperty
+
+    let PlaceholderColor =
+        Attributes.defineAppThemeBindable<Color> InputView.PlaceholderColorProperty
+
+    let TextColor =
+        Attributes.defineAppThemeBindable<Color> InputView.TextColorProperty
+
+    let Text =
+        Attributes.defineBindable<string> InputView.TextProperty
+
+    let Keyboard =
+        Attributes.defineBindable<Keyboard> InputView.KeyboardProperty
+
     let TextChanged =
         Attributes.defineEvent<TextChangedEventArgs>
             "InputView_TextChanged"
             (fun target -> (target :?> InputView).TextChanged)
+
+[<Extension>]
+type InputViewModifiers =
+
+    [<Extension>]
+    static member inline characterSpacing(this: WidgetBuilder<'msg, #IInputView>, value: float) =
+        this.AddScalar(InputView.CharacterSpacing.WithValue(value))
+
+    [<Extension>]
+    static member inline placeholder(this: WidgetBuilder<'msg, #IInputView>, value: string) =
+        this.AddScalar(InputView.Placeholder.WithValue(value))
+
+    [<Extension>]
+    static member inline placeholderColor(this: WidgetBuilder<'msg, #IInputView>, light: Color, ?dark: Color) =
+        this.AddScalar(InputView.PlaceholderColor.WithValue(AppTheme.create light dark))
+
+    [<Extension>]
+    static member inline textColor(this: WidgetBuilder<'msg, #IInputView>, light: Color, ?dark: Color) =
+        this.AddScalar(InputView.TextColor.WithValue(AppTheme.create light dark))
+
+    [<Extension>]
+    static member inline keyboard(this: WidgetBuilder<'msg, #IInputView>, value: Keyboard) =
+        this.AddScalar(InputView.Keyboard.WithValue(value))
