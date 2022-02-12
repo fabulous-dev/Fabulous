6d9e5f19885e8a1d9ee67eac7b5b24474e313425
Edgar <edgar.gonzalez@fundourselves.com>
2022-02-12 14:04:30 +0100

    Add Extension for Cell and TextCell

--- src/Fabulous.XamarinForms/Views/Cells/TextCell.fs
+++ src/Fabulous.XamarinForms/Views/Cells/TextCell.fs
@@ -14,7 +14,13 @@ module TextCell =
         Attributes.defineBindable<string> TextCell.TextProperty
 
     let TextColor =
-        Attributes.defineBindable<Color> TextCell.TextColorProperty
+        Attributes.defineAppThemeBindable<Color> TextCell.TextColorProperty
+
+    let Detail =
+        Attributes.defineBindable<string> TextCell.DetailProperty
+
+    let DetailColor =
+        Attributes.defineAppThemeBindable<Color> TextCell.DetailColorProperty
 
 [<AutoOpen>]
 module TextCellBuilders =
@@ -24,6 +30,22 @@ module TextCellBuilders =
 
 [<Extension>]
 type TextCellModifiers =
+    /// <summary>Set the color of the text.</summary>
+    /// <param name="light">The color of the text in the light theme.</param>
+    /// <param name="dark">The color of the text in the dark theme.</param>
     [<Extension>]
-    static member inline textColor(this: WidgetBuilder<'msg, ITextCell>, value: Color) =
-        this.AddScalar(TextCell.TextColor.WithValue(value))
+    static member inline textColor(this: WidgetBuilder<'msg, ITextCell>, light: Color, ?dark: Color) =
+        this.AddScalar(TextCell.TextColor.WithValue(AppTheme.create light dark))
+
+    /// <summary>Set the color of the detail text.</summary>
+    /// <param name="light">The color of the text in the light theme.</param>
+    /// <param name="dark">The color of the text in the dark theme.</param>
+    [<Extension>]
+    static member inline detailColor(this: WidgetBuilder<'msg, ITextCell>, light: Color, ?dark: Color) =
+        this.AddScalar(TextCell.DetailColor.WithValue(AppTheme.create light dark))
+
+    /// <summary>Set the text of the detail text cell.</summary>
+    /// <param name="text">The text of the detail text cell.</param>
+    [<Extension>]
+    static member inline detailText(this: WidgetBuilder<'msg, ITextCell>, text: string) =
+        this.AddScalar(TextCell.Detail.WithValue(text))
