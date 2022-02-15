59af775399966e97422455a859782d4a4cfc0151
Edgar <edgar.gonzalez@fundourselves.com>
2022-02-15 17:24:56 +0100

    ActivityIndicator + Extensions

--- src/Fabulous.XamarinForms/Views/Controls/ActivityIndicator.fs
+++ src/Fabulous.XamarinForms/Views/Controls/ActivityIndicator.fs
@@ -1,5 +1,6 @@
 namespace Fabulous.XamarinForms
 
+open System.Runtime.CompilerServices
 open Fabulous
 open Xamarin.Forms
 
@@ -12,6 +13,9 @@ module ActivityIndicator =
     let IsRunning =
         Attributes.defineBindable<bool> ActivityIndicator.IsRunningProperty
 
+    let Color =
+        Attributes.defineAppThemeBindable<Color> ActivityIndicator.ColorProperty
+
 [<AutoOpen>]
 module ActivityIndicatorBuilders =
     type Fabulous.XamarinForms.View with
@@ -20,3 +24,13 @@ module ActivityIndicatorBuilders =
                 ActivityIndicator.WidgetKey,
                 ActivityIndicator.IsRunning.WithValue(isRunning)
             )
+
+[<Extension>]
+type ActivityIndicatorModifiers =
+
+    /// <summary>Sets the activity indicator color.</summary>
+    /// <param name="light">The color of the activity indicator in the light theme.</param>
+    /// <param name="dark">The color of the activity indicator in the dark theme.</param>
+    [<Extension>]
+    static member inline color(this: WidgetBuilder<'msg, #IActivityIndicator>, light: Color, ?dark: Color) =
+        this.AddScalar(ActivityIndicator.Color.WithValue(AppTheme.create light dark))
