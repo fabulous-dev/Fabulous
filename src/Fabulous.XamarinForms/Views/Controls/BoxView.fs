814483f300681833bb19f3e90beb6bea007cc88b
Edgar <edgar.gonzalez@fundourselves.com>
2022-02-16 08:49:23 +0100

    Add BoxView and Extensions

--- src/Fabulous.XamarinForms/Views/Controls/BoxView.fs
+++ src/Fabulous.XamarinForms/Views/Controls/BoxView.fs
@@ -1,5 +1,6 @@
 namespace Fabulous.XamarinForms
 
+open System.Runtime.CompilerServices
 open Fabulous
 open Xamarin.Forms
 
@@ -10,10 +11,21 @@ module BoxView =
     let WidgetKey = Widgets.register<BoxView> ()
 
     let Color =
-        Attributes.defineBindable<Color> BoxView.ColorProperty
+        Attributes.defineAppThemeBindable<Color> BoxView.ColorProperty
+
+    let CornerRadius =
+        Attributes.defineBindable<float> BoxView.CornerRadiusProperty
 
 [<AutoOpen>]
 module BoxViewBuilders =
     type Fabulous.XamarinForms.View with
-        static member inline BoxView<'msg>(color: Color) =
-            WidgetBuilder<'msg, IBoxView>(BoxView.WidgetKey, BoxView.Color.WithValue(color))
+        static member inline BoxView<'msg>(light: Color, ?dark: Color) =
+            WidgetBuilder<'msg, IBoxView>(BoxView.WidgetKey, BoxView.Color.WithValue(AppTheme.create light dark))
+
+[<Extension>]
+type BoxViewModifiers =
+    /// <summary>Set the corner radius of the BoxView</summary>
+    /// <param name="value">corner radius value for the box view.</param>
+    [<Extension>]
+    static member inline cornerRadius(this: WidgetBuilder<'msg, #IBoxView>, value: float) =
+        this.AddScalar(BoxView.CornerRadius.WithValue(value))
