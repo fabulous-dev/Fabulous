3c609c99ff79f4664c1ab69e9a7cb2e91a2193fa
Edgar <edgar.gonzalez@fundourselves.com>
2022-03-02 21:49:54 +0000

    SearchBar + Extensions

--- src/Fabulous.XamarinForms/Views/Controls/SearchBar.fs
+++ src/Fabulous.XamarinForms/Views/Controls/SearchBar.fs
@@ -10,11 +10,23 @@ type ISearchBar =
 module SearchBar =
     let WidgetKey = Widgets.register<SearchBar> ()
 
-    let Text =
-        Attributes.defineBindable<string> SearchBar.TextProperty
-
     let CancelButtonColor =
-        Attributes.defineBindable<Color> SearchBar.CancelButtonColorProperty
+        Attributes.defineAppThemeBindable<Color> SearchBar.CancelButtonColorProperty
+
+    let FontAttributes =
+        Attributes.defineBindable<FontAttributes> SearchBar.FontAttributesProperty
+
+    let FontFamily =
+        Attributes.defineBindable<string> SearchBar.FontFamilyProperty
+
+    let FontSize =
+        Attributes.defineBindable<float> SearchBar.FontSizeProperty
+
+    let HorizontalTextAlignment =
+        Attributes.defineBindable<TextAlignment> SearchBar.HorizontalTextAlignmentProperty
+
+    let VerticalTextAlignment =
+        Attributes.defineBindable<TextAlignment> SearchBar.VerticalTextAlignmentProperty
 
     let SearchButtonPressed =
         Attributes.defineEventNoArg
@@ -27,16 +39,61 @@ module SearchBarBuilders =
         static member inline SearchBar<'msg>(text: string, onTextChanged: string -> 'msg, onSearchButtonPressed: 'msg) =
             WidgetBuilder<'msg, ISearchBar>(
                 SearchBar.WidgetKey,
-                SearchBar.Text.WithValue(text),
+                InputView.Text.WithValue(text),
                 InputView.TextChanged.WithValue(fun args -> onTextChanged args.NewTextValue |> box),
                 SearchBar.SearchButtonPressed.WithValue(onSearchButtonPressed)
             )
 
 [<Extension>]
 type SearchBarModifiers =
+    /// <summary>Sets the color of the cancel button text.</summary>
+    /// <param name="light">The color of the cancel button text in the light theme.</param>
+    /// <param name="dark">The color of the cancel button text in the dark theme.</param>
     [<Extension>]
-    static member inline cancelButtonColor(this: WidgetBuilder<'msg, #ISearchBar>, value: Color) =
-        this.AddScalar(SearchBar.CancelButtonColor.WithValue(value))
+    static member inline cancelButtonColor(this: WidgetBuilder<'msg, #ISearchBar>, light: Color, ?dark: Color) =
+        this.AddScalar(SearchBar.CancelButtonColor.WithValue(AppTheme.create light dark))
+
+    [<Extension>]
+    static member inline font
+        (
+            this: WidgetBuilder<'msg, #ISearchBar>,
+            ?size: float,
+            ?namedSize: NamedSize,
+            ?attributes: FontAttributes,
+            ?fontFamily: string
+        ) =
+
+        let mutable res = this
+
+        match size with
+        | None -> ()
+        | Some v -> res <- res.AddScalar(SearchBar.FontSize.WithValue(v))
+
+        match namedSize with
+        | None -> ()
+        | Some v -> res <- res.AddScalar(SearchBar.FontSize.WithValue(Device.GetNamedSize(v, typeof<SearchBar>)))
+
+        match attributes with
+        | None -> ()
+        | Some v -> res <- res.AddScalar(SearchBar.FontAttributes.WithValue(v))
+
+        match fontFamily with
+        | None -> ()
+        | Some v -> res <- res.AddScalar(SearchBar.FontFamily.WithValue(v))
+
+        res
+
+    /// <summary>Set the horizontal text alignment</summary>
+    /// param name="alignment">The horizontal text alignment</summary>
+    [<Extension>]
+    static member inline horizontalTextAlignment(this: WidgetBuilder<'msg, #ISearchBar>, value: TextAlignment) =
+        this.AddScalar(SearchBar.HorizontalTextAlignment.WithValue(value))
+
+    /// <summary>Set the vertical text alignment</summary>
+    /// param name="alignment">The vertical text alignment</summary>
+    [<Extension>]
+    static member inline verticalTextAlignment(this: WidgetBuilder<'msg, #ISearchBar>, value: TextAlignment) =
+        this.AddScalar(SearchBar.VerticalTextAlignment.WithValue(value))
 
     /// <summary>Link a ViewRef to access the direct SearchBar control instance</summary>
     [<Extension>]
