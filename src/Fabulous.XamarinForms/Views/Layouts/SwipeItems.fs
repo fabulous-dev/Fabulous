adfd68a0c82ae79c14fb7476d10eaceb3df602d4
Edgar <edgar.gonzalez@fundourselves.com>
2022-03-18 15:09:52 +0000

    SwipeView + Widget + Extensions

---
+++ src/Fabulous.XamarinForms/Views/Layouts/SwipeItems.fs
@@ -0,0 +1,44 @@
+namespace Fabulous.XamarinForms
+
+open System.Collections.Generic
+open System.Runtime.CompilerServices
+open Fabulous
+open Xamarin.Forms
+open Fabulous.XamarinForms
+
+type ISwipeItems =
+    inherit IElement
+
+module SwipeItems =
+
+    let WidgetKey = Widgets.register<SwipeItems> ()
+
+    let SwipeItems =
+        Attributes.defineWidgetCollection "SwipeItems_SwipeItems" (fun target -> (target :?> SwipeItems) :> IList<_>)
+
+    let SwipeMode =
+        Attributes.defineBindable<SwipeMode> Xamarin.Forms.SwipeItems.ModeProperty
+
+    let SwipeBehaviorOnInvoked =
+        Attributes.defineBindable<SwipeBehaviorOnInvoked> Xamarin.Forms.SwipeItems.SwipeBehaviorOnInvokedProperty
+
+[<AutoOpen>]
+module SwipeItemsBuilders =
+
+    type Fabulous.XamarinForms.View with
+        static member inline SwipeItems<'msg>() =
+            CollectionBuilder<'msg, ISwipeItems, ISwipeItem>(SwipeItems.WidgetKey, SwipeItems.SwipeItems)
+
+[<Extension>]
+type SwipeItemsModifiers() =
+    [<Extension>]
+    static member inline swipeMode(this: WidgetBuilder<'msg, #ISwipeItems>, value: SwipeMode) =
+        this.AddScalar(SwipeItems.SwipeMode.WithValue(value))
+
+    [<Extension>]
+    static member inline swipeBehaviorOnInvoked(this: WidgetBuilder<'msg, #ISwipeItems>, value: SwipeBehaviorOnInvoked) =
+        this.AddScalar(SwipeItems.SwipeBehaviorOnInvoked.WithValue(value))
+
+    [<Extension>]
+    static member inline reference(this: WidgetBuilder<'msg, ISwipeItems>, value: ViewRef<SwipeItems>) =
+        this.AddScalar(ViewRefAttributes.ViewRef.WithValue(value.Unbox))
