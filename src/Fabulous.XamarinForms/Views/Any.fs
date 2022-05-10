a61afebee587829e882f9aa9d2ee97295fc70fe4
Timothé Larivière <timothe.lariviere@outlook.com>
2022-05-10 11:48:03 +0200

    Add helper to return different controls in a same conditional expression

---
+++ src/Fabulous.XamarinForms/Views/Any.fs
@@ -0,0 +1,19 @@
+namespace Fabulous.XamarinForms
+
+open Fabulous
+open Fabulous.XamarinForms
+
+[<AutoOpen>]
+module AnyBuilders =
+    type Fabulous.XamarinForms.View with
+        /// Downcast to IView to allow to return different types of views in a single expression (e.g. if/else, match with pattern, etc.)
+        static member AnyView<'msg, 'marker when 'marker :> IView>(widget: WidgetBuilder<'msg, 'marker>) =
+            WidgetBuilder<'msg, IView>(widget.Key, widget.Attributes)
+
+        /// Downcast to IPage to allow to return different types of pages in a single expression (e.g. if/else, match with pattern, etc.)
+        static member AnyPage<'msg, 'marker when 'marker :> IPage>(widget: WidgetBuilder<'msg, 'marker>) =
+            WidgetBuilder<'msg, IPage>(widget.Key, widget.Attributes)
+
+        /// Downcast to ICell to allow to return different types of cells in a single expression (e.g. if/else, match with pattern, etc.)
+        static member AnyCell<'msg, 'marker when 'marker :> ICell>(widget: WidgetBuilder<'msg, 'marker>) =
+            WidgetBuilder<'msg, ICell>(widget.Key, widget.Attributes)
