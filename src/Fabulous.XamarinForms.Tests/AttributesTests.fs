ede6229aaaac7df3212093f322877e6426489250
Timothé Larivière <timothe.lariviere@outlook.com>
2022-05-16 16:23:57 +0200

    Fix LayoutOptions.encode/decode and add unit tests for it

---
+++ src/Fabulous.XamarinForms.Tests/AttributesTests.fs
@@ -0,0 +1,17 @@
+﻿namespace Fabulous.XamarinForms.Tests
+
+open Fabulous.XamarinForms
+open FsCheck
+open NUnit.Framework
+open FsCheck.NUnit
+open Xamarin.Forms   
+
+[<TestFixture>]
+type ``Small scalar encode tests``() =    
+    [<Property>]
+    member _.``Encoding then decoding a LayoutOptions should return an identical LayoutOptions`` (value: LayoutOptions) =
+        let encoded = SmallScalars.LayoutOptions.encode value
+        let decoded = SmallScalars.LayoutOptions.decode encoded
+        Assert.AreEqual(value.Alignment, decoded.Alignment)
+        Assert.AreEqual(value.Expands, decoded.Expands)
+    
---
+++ src/Fabulous.XamarinForms.Tests/Generators.fs
@@ -0,0 +1,22 @@
+namespace Fabulous.XamarinForms.Tests
+
+open FsCheck
+open NUnit.Framework
+open Xamarin.Forms
+
+module SmallScalarGenerators =
+    let layoutOptionsGenerator = gen {
+        let! layoutAlignment = Gen.elements [ LayoutAlignment.Start; LayoutAlignment.Center; LayoutAlignment.End; LayoutAlignment.Fill ]
+        let! expands = Arb.generate<bool>
+        return LayoutOptions(layoutAlignment, expands)
+    }
+    
+type Generators =
+    static member LayoutOptions() =
+        { new Arbitrary<LayoutOptions>() with
+            override this.Generator = SmallScalarGenerators.layoutOptionsGenerator }     
+
+[<SetUpFixture>]
+type Setup() =
+    member _.Setup() =
+        do Arb.register<Generators>() |> ignore
