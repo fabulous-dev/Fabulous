30838fcac6fcf72830b0aa1d858de24e2bbd06fd
Edgar <edgar.gonzalez@fundourselves.com>
2022-09-07 17:26:42 +0200

    Update xml docs

--- src/Fabulous.MauiControls/Views/Brushes/RadialGradientBrush.fs
+++ src/Fabulous.MauiControls/Views/Brushes/RadialGradientBrush.fs
@@ -21,9 +21,9 @@ module RadialGradientBrush =
 [<AutoOpen>]
 module RadialGradientBrushBuilders =
     type Fabulous.Maui.View with
-        /// <summary>RadialGradientBrush paints an area with a linear gradient, which blends two or more colors along a line known as the gradient axis. </summary>
-        /// <param name="endPoint">EndPoint, of type Point, which represents the ending two-dimensional coordinates of the linear gradient. The default value of this property is (1,1).</param>
-        /// <param name="startPoint">StartPoint, of type Point, which represents the starting two-dimensional coordinates of the linear gradient. The default value of this property is (0,0).</param>
+        /// <summary>RadialGradientBrush paints an area with a radial gradient, which blends two or more colors across a circle.</summary>
+        /// <param name="center">Center, of type Point, which represents the center point of the circle for the radial gradient. The default value of this property is (0.5,0.5).</param>
+        /// <param name="radius">Radius, of type float, which represents the radius of the circle for the radial gradient. The default value of this property is 0.5.</param>
         static member inline RadialGradientBrush<'msg>(?center: Point, ?radius: float) =
             match center, radius with
             | Some c, None ->
