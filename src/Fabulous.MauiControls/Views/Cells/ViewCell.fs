e7f6aca2d558bbe0d464288885c8fc006c91aa88
Edgar <edgar.gonzalez@fundourselves.com>
2022-10-17 17:11:35 +0200

    Format code

--- src/Fabulous.MauiControls/Views/Cells/ViewCell.fs
+++ src/Fabulous.MauiControls/Views/Cells/ViewCell.fs
@@ -19,7 +19,9 @@ module ViewCell =
 [<AutoOpen>]
 module ViewCellBuilders =
     type Fabulous.Maui.View with
-        static member inline ViewCell<'msg, 'marker when 'marker :> Fabulous.Maui.IView>(view: WidgetBuilder<'msg, 'marker>) =
+        static member inline ViewCell<'msg, 'marker when 'marker :> Fabulous.Maui.IView>
+            (view: WidgetBuilder<'msg, 'marker>)
+            =
             WidgetHelpers.buildWidgets<'msg, IViewCell> ViewCell.WidgetKey [| ViewCell.View.WithValue(view.Compile()) |]
 
 [<Extension>]
