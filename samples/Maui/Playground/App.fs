cee0b12a2c174c2568e4ccb21e5c00dbf398599b
Edgar <edgar.gonzalez@fundourselves.com>
2022-11-14 07:27:02 +0100

    Add ZIndex support

--- samples/Maui/Playground/App.fs
+++ samples/Maui/Playground/App.fs
@@ -41,29 +41,51 @@ module App =
         Application(
             ContentPage(
                 "Title",
-                (VStack(spacing = 20.) {
-                    let text =
-                        match model.Focus with
-                        | None -> "None"
-                        | Some f -> f.ToString()
+                (VStack() {
+                    // let text =
+                    //     match model.Focus with
+                    //     | None -> "None"
+                    //     | Some f -> f.ToString()
+                    //
+                    // Label($"Field currently selected: {text}")
+                    //
+                    // Entry("Entry1", TextChanged)
+                    //     .focus(model.Focus = Some Entry1, focusChanged Entry1)
+                    //
+                    // Entry("Entry2", TextChanged)
+                    //     .focus(model.Focus = Some Entry2, focusChanged Entry2)
+                    //
+                    // Entry("Entry3", TextChanged)
+                    //     .focus(model.Focus = Some Entry3, focusChanged Entry3)
+                    //     
+                    (Grid(coldefs = [Star], rowdefs = [Star]) {
+                        Label("View 1")
+                            .zindex(4)
+                            .backgroundColor(Colors.Green.ToFabColor())
+                            .margin(10., 0., 0.,0.)
+                            
+                        Label("View 2")
+                            .zindex(3)
+                            .backgroundColor(Colors.Navy.ToFabColor())
+                            .margin(30., 30., 0.,0.)
+                            
+                        Label("View 3")
+                            .zindex(2)
+                            .backgroundColor(Colors.Yellow.ToFabColor())
+                            .margin(60., 60., 0.,0.)
+                            
+                        Label("View 4")
+                            .zindex(1)
+                            .backgroundColor(Colors.SkyBlue.ToFabColor())
+                            .margin(90., 90., 0.,0.)
+                    }).centerVertical()
 
-                    Label($"Field currently selected: {text}")
-
-                    Entry("Entry1", TextChanged)
-                        .focus(model.Focus = Some Entry1, focusChanged Entry1)
-
-                    Entry("Entry2", TextChanged)
-                        .focus(model.Focus = Some Entry2, focusChanged Entry2)
-
-                    Entry("Entry3", TextChanged)
-                        .focus(model.Focus = Some Entry3, focusChanged Entry3)
-
-                    Button("Set focus on Entry1", SetFocus(Some Entry1))
-                    Button("Set focus on Entry2", SetFocus(Some Entry2))
-                    Button("Set focus on Entry3", SetFocus(Some Entry3))
-                    Button("Unfocus", SetFocus None)
+                    // Button("Set focus on Entry1", SetFocus(Some Entry1))
+                    // Button("Set focus on Entry2", SetFocus(Some Entry2))
+                    // Button("Set focus on Entry3", SetFocus(Some Entry3))
+                    // Button("Unfocus", SetFocus None)
                  })
-                    .margin(20.)
+                    .padding(20.)
             )
         )
 
--- src/Fabulous.MauiControls/Views/_VisualElement.fs
+++ src/Fabulous.MauiControls/Views/_VisualElement.fs
@@ -181,6 +181,9 @@ module VisualElement =
 
     let ScaleX =
         Attributes.defineBindableFloat VisualElement.ScaleXProperty
+        
+    let ZIndex =
+        Attributes.defineBindableInt VisualElement.ZIndexProperty
 
     let ScaleTo =
         Attributes.defineSimpleScalarWithEquality<ScaleToData>
@@ -568,3 +571,7 @@ type VisualElementModifiers =
                   Easing = easing }
             )
         )
+        
+    [<Extension>]
+    static member inline zindex(this: WidgetBuilder<'msg, #IVisualElement>, value: int) =
+        this.AddScalar(VisualElement.ZIndex.WithValue(value))
