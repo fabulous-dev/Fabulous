3fc4106f41b029aff17feb848533f8f32edb3081
Edgar <edgar.gonzalez@fundourselves.com>
2022-11-16 07:54:33 +0100

    Fix warning deprecations

--- src/Fabulous.MauiControls/Async.fs
+++ src/Fabulous.MauiControls/Async.fs
@@ -1,12 +1,12 @@
 namespace Fabulous.Maui
 
-open Microsoft.Maui.Controls
+open Microsoft.Maui.ApplicationModel
 
 module Async =
     /// Execute the async computation on the device main thread
     let executeOnMainThread (action: Async<'T>) : Async<'T> =
         async {
             return!
-                Device.InvokeOnMainThreadAsync(funcTask = fun () -> Async.StartImmediateAsTask action)
+                MainThread.InvokeOnMainThreadAsync(funcTask = fun () -> Async.StartImmediateAsTask action)
                 |> Async.AwaitTask
         }
