3865cdc8b0d4b5cdf0f8af565ba8967bb218b9df
Timothé LARIVIERE <timothe.lariviere@outlook.com>
2023-01-31 23:11:02 +0100

    Add unit test + add changelog entry

---
+++ src/Fabulous.Tests/ViewTests.fs
@@ -0,0 +1,36 @@
+namespace Fabulous.Tests
+
+open Fabulous
+open Fabulous.StackAllocatedCollections.StackList
+open NUnit.Framework
+open FsCheck.NUnit
+
+type ITestControl =
+    interface
+    end
+
+module TestControl =
+    let WidgetKey: WidgetKey = 1
+
+type Msg = ChildMsg of unit
+
+[<TestFixture>]
+type ``View helper functions tests``() =
+
+    /// Test: https://github.com/fabulous-dev/Fabulous/pull/1037
+    [<Test>]
+    member _.``Mapping a WidgetBuilder with Unit Msg to another Msg is supported``() =
+        let widgetBuilder =
+            WidgetBuilder<unit, ITestControl>(TestControl.WidgetKey, AttributesBundle(StackList.empty(), ValueNone, ValueNone))
+
+        let mapMsg (oldMsg: unit) = ChildMsg oldMsg
+
+        let mappedWidgetBuilder = View.map mapMsg widgetBuilder
+
+        Assert.AreEqual(widgetBuilder.Key, mappedWidgetBuilder.Key)
+
+        let struct (scalars, _, _) = mappedWidgetBuilder.Attributes
+        let scalars = StackList.toArray &scalars
+
+        Assert.AreEqual(1, scalars.Length)
+        Assert.AreEqual(MapMsg.MapMsg.Key, scalars[0].Key)
