e377841dd2732ef30142b292c48e8ac4cab7f116
Holger Schmidt <leroyz.mailaccount@gmx.net>
2024-03-01 21:34:58 +0100

    simplified debounce test adding message counting

--- src/Fabulous.Tests/CmdTests.fs
+++ src/Fabulous.Tests/CmdTests.fs
@@ -13,13 +13,14 @@ module CmdTestsHelper =
 [<TestFixture>]
 type ``Cmd tests``() =
     [<Test>]
-    member _.``Cmd.debounce only dispatch the last message``() =
+    member _.``Cmd.debounce only dispatches the last messages within the timeout``() =
         async {
+            let mutable messageCount = 0
             let mutable actualValue = None
 
             let dispatch msg =
-                if actualValue.IsNone then
-                    actualValue <- Some msg
+                messageCount <- messageCount + 1
+                actualValue <- Some msg
 
             let triggerCmd = Cmd.debounce 100 NewValue
 
@@ -30,14 +31,14 @@ type ``Cmd tests``() =
             triggerCmd 3 |> CmdTestsHelper.execute dispatch
             do! Async.Sleep 125
 
+            Assert.AreEqual(1, messageCount)
             Assert.AreEqual(Some(NewValue 3), actualValue)
-
-            actualValue <- None
 
             triggerCmd 4 |> CmdTestsHelper.execute dispatch
             do! Async.Sleep 75
             triggerCmd 5 |> CmdTestsHelper.execute dispatch
             do! Async.Sleep 125
 
+            Assert.AreEqual(2, messageCount)
             Assert.AreEqual(Some(NewValue 5), actualValue)
         }
